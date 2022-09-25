df59e72ca3d5a473e8ba78279851081754f69c27
hyunsb <jhss0113@naver.com>
2022-09-26 01:47:57 +0900

    Algorithm test day3

--- src/Level1/StringSortReverse.java
+++ src/Level1/StringSortReverse.java
@@ -1,5 +1,8 @@
 package Level1;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StringSortReverse {
     public static void main(String[] args) {
         String str = "Hello, world!";
@@ -7,19 +10,24 @@ public class StringSortReverse {
         //Use StringBuffer 문자열 자체를 역순으로 정렬
         String reverseBuffer = new StringBuffer(str).reverse().toString();
 
-        //Use Stream :: JAVA8 문자열의 각 문자를 비교하여 역순으로 정렬
+        //Use Stream :: JAVA8 문자열의 각 문자를 비교하여 오름차순으로 정렬
         String reverseStream = str.chars().sorted().collect(
                                                         StringBuilder::new,
                                                         StringBuilder::appendCodePoint,
                                                         StringBuilder::append).toString();
 
-        //use Arrays lib 문자열의 각 문자를 비교하여 역순으로 정렬
+        //use Arrays lib 문자열의 각 문자를 비교하여 내림차순으로 정렬
         String[] strArr = str.split("");
         Arrays.sort(strArr, Collections.reverseOrder());
         String reverseArrays = String.join("", strArr);
 
+        String reverseStream2 = Stream.of(str.split(""))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining());
+
         System.out.println(reverseBuffer);
         System.out.println(reverseStream);
         System.out.println(reverseArrays);
+        System.out.println(reverseStream2);
     }
 }
---
+++ src/Level1/StringToInt.java
@@ -0,0 +1,14 @@
+package Level1;
+
+public class StringToInt {
+    public static void main(String[] args) {
+        String s = "a234";
+
+        // My solved
+        int strLen = s.length();
+        boolean answer = (strLen == 4 || strLen ==6) ? true:false;
+        if(answer == true) answer = (s.replaceAll("[^0-9]","").length() == strLen)? true:false;
+
+        System.out.println(answer);
+    }
+}
