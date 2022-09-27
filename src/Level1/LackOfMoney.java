59940e72518c8b0cc0c7694cec9818e5981320c8
hyunsb <jhss0113@naver.com>
2022-09-27 22:52:01 +0900

    Algorithm test day4

---
+++ src/Level1/LackOfMoney.java
@@ -0,0 +1,14 @@
+package Level1;
+
+public class LackOfMoney {
+    public static void main(String[] args) {
+        int price, money, count;
+        price = 3;
+        money = 20;
+        count = 4;
+
+        long answer = 0;
+
+
+    }
+}
