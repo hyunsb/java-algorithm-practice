8821ef6f8193ca2c41d706938e28e0b9dc8175ec
hyunsb <jhss0113@naver.com>
2022-09-28 18:09:29 +0900

    Algorithm test day5

---
+++ src/Level1/ArrayMinMax.java
@@ -0,0 +1,19 @@
+package Level1;
+
+import java.util.Arrays;
+public class ArrayMinMax {
+    public static void main(String[] args) {
+        String str1 = "1 2 3 4";
+        String str2 = "-1 -2 -3 -4";
+
+        String[] strArr = str1.split(" ");
+
+        int[] intArr = Arrays.stream(strArr).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(intArr);
+
+        System.out.println(String.valueOf(intArr[0]) + " " +
+                String.valueOf(intArr[intArr.length-1]));
+
+
+    }
+}
