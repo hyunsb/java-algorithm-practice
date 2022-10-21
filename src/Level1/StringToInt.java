b2f72ba689b81c04b59f6f72415d197b3f2bce30
hyunsb <jhss0113@naver.com>
2022-10-22 01:56:55 +0900

    Algorithm test day13

--- src/Level1/StringToInt.java
+++ src/Level1/StringToInt.java
@@ -5,10 +5,14 @@ public class StringToInt {
         String s = "a234";
 
         // My solved
+        System.out.println(solution(s));
+    }
+
+    public static boolean solution(String s){
         int strLen = s.length();
         boolean answer = (strLen == 4 || strLen ==6) ? true:false;
         if(answer == true) answer = (s.replaceAll("[^0-9]","").length() == strLen)? true:false;
 
-        System.out.println(answer);
+        return answer;
     }
 }
