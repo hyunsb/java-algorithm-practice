2e118812a5261dcf3c65d02b998a24ce3e8fe481
hyunsb <jhss0113@naver.com>
2022-10-21 23:43:18 +0900

    Algorithm test day13

--- src/Level1/PrintStar.java
+++ src/Level1/PrintStar.java
@@ -8,6 +8,10 @@ public class PrintStar {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
+        solution(a, b);
+    }
+
+    public static void solution(int a, int b){
         for(int i=1; i<=b; i++){
             for(int j=1; j<=a; j++){
                 System.out.print("*");
