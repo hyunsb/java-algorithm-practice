688d8f59aaf415de7b21d3b2584e87202e16d104
hyunsb <jhss0113@naver.com>
2022-10-28 01:03:58 +0900

    create problem4

---
+++ src/woow/problem4.java
@@ -0,0 +1,12 @@
+package woow;
+
+public class problem4 {
+    public static void main(String[] args) {
+
+        String word = "I love you";
+        char[] list = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M',
+                'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+
+    }
+    public static
+}
