a895dee3e4c5ddc50f250b49d5ea17626db4bfd9
hyunsb <jhss0113@naver.com>
2022-10-27 20:43:25 +0900

    woow problem1

--- src/woow/problem1.java
+++ src/woow/problem1.java
@@ -1,5 +1,41 @@
 package woow;
 
-public class problem1 {
+import java.util.List;
 
+public class problem1 {
+    public static void main(String[] args) {
+        List<Integer> pobi = List.of(97, 98);
+        List<Integer> crong = List.of(197, 198);
+
+        System.out.println(solution(pobi,crong));
+
+    }
+
+    public static int solution(List<Integer> pobi, List<Integer> crong){
+        int answer = Integer.MAX_VALUE;
+
+        if(pobi.get(0) == pobi.get(1)-1 && crong.get(0) == crong.get(1)-1){
+            int pobiMax = Math.max(findMaxNum(pobi.get(0)), findMaxNum(pobi.get(1)));
+            int crongMax = Math.max(findMaxNum(crong.get(0)), findMaxNum(crong.get(1)));
+            if(pobiMax == crongMax) return 0;
+            return (pobiMax > crongMax) ? 1 : 2;
+        }else{
+            return -1;
+        }
+    }
+
+    public static int findMaxNum(int n) {
+        int add = n % 10;
+        int mul = n % 10;
+        n /= 10;
+
+        while(n > 0) {
+            int temp = n % 10;
+            add += temp;
+            mul *= temp;
+            n /= 10;
+        }
+
+        return Math.max(add, mul);
+    }
 }
