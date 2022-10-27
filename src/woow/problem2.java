1816498a6c74d84942839fbf2575a96f5e9cb020
hyunsb <jhss0113@naver.com>
2022-10-27 23:11:30 +0900

    woow problem2

--- src/woow/problem2.java
+++ src/woow/problem2.java
@@ -1,19 +1,23 @@
 package woow;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.ArrayList;
 
 public class problem2 {
     public static void main(String[] args) {
         String cryptogram = "browoanoommnaon";
+
+        ArrayList<String> list = new ArrayList<String>();
+        for(int i=0; i < cryptogram.length(); i++){
+            if(cryptogram.charAt(i) != cryptogram.charAt(i+1)){
+
+            }
+        }
 
 
     }
 
     public static String solution(String cryptogram){
         String answer = "";
-
 
         return answer;
     }
