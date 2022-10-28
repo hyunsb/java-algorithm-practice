2d1238e940c3428b0eb94cb373fbfbf94b127f23
hyunsb <jhss0113@naver.com>
2022-10-28 15:20:15 +0900

    ploblem4 solved

--- src/woow/problem4.java
+++ src/woow/problem4.java
@@ -1,12 +1,34 @@
 package woow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class problem4 {
     public static void main(String[] args) {
 
         String word = "I love you";
-        char[] list = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M',
-                'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+        String answer = "";
 
+        char[] code = {'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P',
+                'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+
+        for(int i=0; i < word.length(); i++){
+            char tmp = word.charAt(i);
+            if( (65 <= tmp) && (tmp <= 90)){
+                answer += code[tmp-65];
+            } else if ((97 <= tmp) && (tmp <= 122)) {
+                answer += Character.toString(code[tmp-97]).toLowerCase();
+            } else {
+                answer += tmp;
+            }
+        }
+
+        System.out.println(answer);
     }
-    public static
+
+    public static String sol1(String str){
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        return "";
+    }
+
 }
