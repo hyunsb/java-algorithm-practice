1b48df7ba1cf9bef7d94d13fd545afb808ed3da3
hyunsb <jhss0113@naver.com>
2022-10-28 16:50:43 +0900

    create problem5

---
+++ src/woow/problem5.java
@@ -0,0 +1,16 @@
+package woow;
+
+import java.util.Collections;
+import java.util.List;
+
+public class problem5 {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> sol1(int money){
+        List<Integer> answer = Collections.emptyList();
+
+        return answer;
+    }
+}
