2a081b19ffebf2acf92985582554b3b802ef45b0
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-10-30 01:31:14 +0900

    section 1-2 sol2 update

--- InflearnJavaAlgorithm/Section1_String/_2_To_Upper_Lower_Case.java
+++ InflearnJavaAlgorithm/Section1_String/_2_To_Upper_Lower_Case.java
@@ -16,10 +16,25 @@ public class _2_To_Upper_Lower_Case {
         return c;
     }
 
+    public String solution2(String str){
+        String answer = "";
+        for (char x : str.toCharArray()){
+            answer +=  upperToLow2(x);
+        }
+        return answer;
+    }
+
+    public char upperToLow2(char x){
+        if(Character.isLowerCase(x)) return Character.toUpperCase(x);
+        if(Character.isUpperCase(x)) return Character.toLowerCase(x);
+        return x;
+    }
+
     public static void main(String[] args) {
         _2_To_Upper_Lower_Case T = new _2_To_Upper_Lower_Case();
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         System.out.println(T.solution(str));
+        System.out.println(T.solution2(str));
     }
 }
