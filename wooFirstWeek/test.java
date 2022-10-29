e174dd6adef0047e83f76f01b3804cff54b21133
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-10-29 23:01:41 +0900

    problem7 solved

--- wooFirstWeek/test.java
+++ wooFirstWeek/test.java
@@ -19,10 +19,10 @@ public class test {
         System.out.println(solution(user, friends, visitors));
     }
 
-    //하드 코딩
+    //잘 모르겠어서 일단 주먹구구식 하드 코딩 start!
+    //일단 풀고 나중에 배우면서 리팩토링 합시다! 부족한 것을 부끄러워하지 말고 배울 수 있는 기회가 있다고 생각하자!
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-
+        // 각 사람의 친구 목록을 저장할 해시맵
         HashMap<String, List<String>> friendList = new HashMap<String, List<String>>();
 
         for(List<String> list : friends){
@@ -59,6 +59,7 @@ public class test {
         System.out.println(friendList);
 
         List<String> userFriends = friendList.get(user);
+
         for(String str : userFriends){
             scoreList.put(str, 0);
             // str의 친구 리스트에 들어가서 mrko가 없으면 10점추가 mrko가 있으면 0점
@@ -68,12 +69,35 @@ public class test {
                 System.out.println(str2);
                 if(str2.equals(user)) break;
                 if(!friendList.containsValue(user)){
+                    // 스코어 리스트에 존재할 때 +10점
                     if(scoreList.containsKey(str2)) scoreList.put(str2, scoreList.get(str2) + 10);
+                    // 스코어 리스트에 존재하지 않으면 추가
                     if(!scoreList.containsKey(str2)) scoreList.put(str2, 10);
                 }
             }
         }
 
+        System.out.println(scoreList);
+
+        for(String visitor : visitors){
+            if(!friendList.get(user).contains(visitor)){
+                if(scoreList.containsKey(visitor)) scoreList.put(visitor, scoreList.get(visitor) + 1);
+                if(!scoreList.containsKey(visitor)) scoreList.put(visitor, 1);
+            }
+        }
+
+        System.out.println(scoreList);
+
+        List<String> removeList = new ArrayList<>();
+        for(String key : scoreList.keySet()){
+            if(scoreList.get(key) == 0) removeList.add(key);
+        }
+
+        for(String key : removeList){
+            scoreList.remove(key);
+        }
+
+        List<String> answer = new ArrayList<>(scoreList.keySet());
         return answer;
     }
 }
