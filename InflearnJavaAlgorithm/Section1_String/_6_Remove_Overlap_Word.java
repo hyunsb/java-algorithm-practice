16fce764b0c51e88fed26a4887a4d3067a72157a
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-10-30 23:21:43 +0900

    section 1-6 solved

---
+++ InflearnJavaAlgorithm/Section1_String/_6_Remove_Overlap_Word.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class _6_Remove_Overlap_Word {
+    public String solution(String word){
+        Set<String> set = new HashSet<>();
+        for(String str : word.split("")) set.add(str);
+
+        String answer = String.join("", set);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        _6_Remove_Overlap_Word T = new _6_Remove_Overlap_Word();
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        System.out.println(T.solution(word));
+    }
+}
