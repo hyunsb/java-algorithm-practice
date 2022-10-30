36a06aa59b9ea3bf888b9f2796a1330335dd2ba7
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-10-30 23:52:36 +0900

    section 1-7 solved

--- InflearnJavaAlgorithm/Section1_String/_7_Is_Palindrome_Word.java
+++ InflearnJavaAlgorithm/Section1_String/_7_Is_Palindrome_Word.java
@@ -1,8 +1,18 @@
 import java.util.*;
 public class _7_Is_Palindrome_Word {
     public String solution(String word){
-        String answer = "";
-        return answer;
+        String lWord = "", rWord="";
+        int lt = 0, rt = word.length()-1;
+        char[] charList = word.toCharArray();
+
+        while (lt < rt) {
+            lWord += charList[lt];
+            rWord += charList[rt];
+            lt++;
+            rt--;
+        }
+
+        return lWord.equalsIgnoreCase(rWord) ? "YES" : "NO";
     }
     public static void main(String[] args) {
         _7_Is_Palindrome_Word T = new _7_Is_Palindrome_Word();
