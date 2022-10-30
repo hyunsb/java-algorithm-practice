8e35b5ddb1bf327ac0cb0eaf89cd3669be7825fa
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-10-30 21:56:11 +0900

    section 1-4 update

---
+++ InflearnJavaAlgorithm/Section1_String/_4_Reverse_Word.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class _4_Reverse_Word {
+    public ArrayList<String> solution(int n, String[] strings) {
+        ArrayList<String> answer = new ArrayList<String>();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        _4_Reverse_Word T = new _4_Reverse_Word();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] strings = new String[n];
+
+        for(int i=0; i < n; i++){
+            strings[i] = sc.next();
+        }
+
+        for(String str : T.solution(n, strings)){
+            System.out.println(str);
+        }
+    }
+}
