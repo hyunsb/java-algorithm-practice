a0b2b8a3f085e4072d7d7d2f95a5a8b8e9b4badf
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-10-30 23:46:26 +0900

    problem7 solved

---
+++ InflearnJavaAlgorithm/Section1_String/_7_Is_Palindrome_Word.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class _7_Is_Palindrome_Word {
+    public String solution(String word){
+        String answer = "";
+        return answer;
+    }
+    public static void main(String[] args) {
+        _7_Is_Palindrome_Word T = new _7_Is_Palindrome_Word();
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        System.out.println(T.solution(word));
+    }
+}
