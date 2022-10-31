b0886e72ffef3f26b6f948247bf7d25d45479e97
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-10-31 19:28:18 +0900

    section 1-8 sol2 update

--- InflearnJavaAlgorithm/Section1_String/_8_Valid_Palindrome.java
+++ InflearnJavaAlgorithm/Section1_String/_8_Valid_Palindrome.java
@@ -14,11 +14,18 @@ public class _8_Valid_Palindrome {
         return answer;
     }
 
+    public String solution2(String string){
+        string = string.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(string).reverse().toString();
+        return string.equals(tmp) ? "YES" : "NO";
+    }
+
 
     public static void main(String[] args) {
         _8_Valid_Palindrome T = new _8_Valid_Palindrome();
         Scanner sc = new Scanner(System.in);
         String string = sc.nextLine();
         System.out.println(T.solution(string));
+        System.out.println(T.solution2(string));
     }
 }
