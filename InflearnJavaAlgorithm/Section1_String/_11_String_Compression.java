fdabb1f3702ce3f8a58f3a6bf2703e5c99d373b2
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-01 02:28:16 +0900

    section 1-11 solved(sol2, sol3 update) 리팩토링 필요

--- InflearnJavaAlgorithm/Section1_String/_11_String_Compression.java
+++ InflearnJavaAlgorithm/Section1_String/_11_String_Compression.java
@@ -16,7 +16,35 @@ public class _11_String_Compression {
             }
             if (str.charAt(i) == str.charAt(i+1)) cnt++;
         }
+        return answer.toString();
+    }
 
+    public String solution2(String str){
+        StringBuilder answer = new StringBuilder();
+        str += " ";
+        int cnt = 1;
+        for(int i = 0; i < str.length()-1; i++){
+            if(str.charAt(i) == str.charAt(i+1)) cnt++;
+            else {
+                answer.append( (cnt == 1) ? str.charAt(i) : str.charAt(i)+ String.valueOf(cnt));
+                cnt = 1;
+            }
+        }
+        return answer.toString();
+    }
+
+    public String solution3(String str){
+        StringBuilder answer = new StringBuilder();
+        str += " ";
+        int cnt = 1;
+        for(int i = 0; i < str.length()-1; i++){
+            if(str.charAt(i) != str.charAt(i+1)) {
+                answer.append(str.charAt(i));
+                if(cnt>1) answer.append(String.valueOf(cnt));
+                cnt = 1; continue;
+            }
+            cnt++;
+        }
         return answer.toString();
     }
 
@@ -25,5 +53,7 @@ public class _11_String_Compression {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         System.out.println(T.solution(str));
+        System.out.println(T.solution2(str));
+        System.out.println(T.solution3(str));
     }
 }
