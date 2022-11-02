058a1388516b7266fa49c4c1c8b9f38464977497
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-03 02:36:07 +0900

    reset

--- Programmers_Level1/ArraySumNum.java
+++
@@ -1,9 +0,0 @@
-package Level1;
-
-public class ArraySumNum {
-    public int solution(int[] numbers) {
-        int answer = 45;
-        for(int i : numbers) answer -= i;
-        return answer;
-    }
-}
