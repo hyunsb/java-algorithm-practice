690ced000ffb429dba5deec43a2c512bf09264ca
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-02 22:28:25 +0900

    Refactor : Rename Method (selectRandomNumbers)

--- wooSecondWeek/Baseball.java
+++ wooSecondWeek/Baseball.java
@@ -7,20 +7,21 @@ public class Baseball {
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
         // TODO: 프로그램 구현
-        List<Integer> computer = baseball.computerSelectNumbers();
+        List<Integer> computer = baseball.SelectRandomNumbers();
+
         System.out.println(computer);
     }
 
     // TODO : 컴퓨터가 숫자를 선택하는 메소드
-    public List<Integer> computerSelectNumbers() {
-        List<Integer> computerNumbers = new ArrayList<>();
-        while (computerNumbers.size() < 3) {
+    public List<Integer> SelectRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerNumbers.contains(randomNumber)){
-                computerNumbers.add(randomNumber);
+            if (!numbers.contains(randomNumber)){
+                numbers.add(randomNumber);
             }
         }
-        return computerNumbers;
+        return numbers;
     }
 
 }
