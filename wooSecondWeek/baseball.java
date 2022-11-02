6f6916290a4967ea78d5b64236eb7f3d4b9ac6bd
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-02 21:42:46 +0900

    add method (컴퓨터가 서로다른 임의의 숫자 3개를 선택하여 저장하는 메소드)

--- wooSecondWeek/baseball.java
+++ wooSecondWeek/baseball.java
@@ -1,4 +1,26 @@
 package wooSecondWeek;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class baseball {
+    public static void main(String[] args) {
+        baseball baseball = new baseball();
+        // TODO: 프로그램 구현
+        List<Integer> computer = baseball.computerSelectNumbers();
+        System.out.println(computer);
+    }
+
+    // TODO : 컴퓨터가 숫자를 선택하는 메소드
+    public List<Integer> computerSelectNumbers() {
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)){
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
+
 }
