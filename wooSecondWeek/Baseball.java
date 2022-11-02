669a585114f3b830dd70955d8b3c67f871d2798d
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-02 22:40:28 +0900

    Feat : 우테코 라이브러리 추가

--- wooSecondWeek/Baseball.java
+++ wooSecondWeek/Baseball.java
@@ -7,13 +7,13 @@ public class Baseball {
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
         // TODO: 프로그램 구현
-        List<Integer> computer = baseball.SelectRandomNumbers();
+        List<Integer> computer = baseball.selectRandomNumbers();
 
         System.out.println(computer);
     }
 
     // TODO : 컴퓨터가 숫자를 선택하는 메소드
-    public List<Integer> SelectRandomNumbers() {
+    public List<Integer> selectRandomNumbers() {
         List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -23,5 +23,8 @@ public class Baseball {
         }
         return numbers;
     }
+
+    // TODO : 사용자가 숫자를 선택하는 메소드
+    public
 
 }
