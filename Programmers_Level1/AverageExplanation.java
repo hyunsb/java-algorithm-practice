776a51f68a2b1cdaca755b410ea1f5302a8ae1b9
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-03 02:32:01 +0900

    reset

--- Programmers_Level1/AverageExplanation.java
+++
@@ -1,9 +0,0 @@
-package Level1;
-
-import java.util.Arrays;
-
-public class AverageExplanation {
-    public double solution(int[] arr) {
-        return (int) Arrays.stream(arr).average().orElse(0);
-    }
-}
