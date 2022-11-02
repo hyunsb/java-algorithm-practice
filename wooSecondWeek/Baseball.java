b9d842352aa1a3cff05f77a61882210f710a6b5e
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-03 01:27:47 +0900

    Feat : 사용자로부터 숫자를 입력받는 기능 추가 missionutils.Console의 readLine()을 사용하여 입력받음 서로 다른 임의의 수 3개를 입력받음 입력받은 수가 3개가 아니거나, 0을 포함할 시 IllegalArgumentException 발생

--- wooSecondWeek/Baseball.java
+++ wooSecondWeek/Baseball.java
@@ -1,18 +1,23 @@
 package wooSecondWeek;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Baseball {
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
         // TODO: 프로그램 구현
         List<Integer> computer = baseball.selectRandomNumbers();
-
         System.out.println(computer);
+
+        List<Integer> user = baseball.selectNumbers();
+        System.out.println(user);
+
     }
 
-    // TODO : 컴퓨터가 숫자를 선택하는 메소드
+    // TODO : 컴퓨터가 숫자를 랜덤으로 선택하여 리턴
     public List<Integer> selectRandomNumbers() {
         List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < 3) {
@@ -24,7 +29,20 @@ public class Baseball {
         return numbers;
     }
 
-    // TODO : 사용자가 숫자를 선택하는 메소드
-    public
+    // TODO : 사용자가 숫자를 선택한 숫자를 리턴
+    public List<Integer> selectNumbers(){
+        String inputNumber = Console.readLine();
+
+        // TODO : 사용자가 잘못된 값을 입력할 시 IllegalArgumentException 발생
+        if (inputNumber.length() != 3 || inputNumber.contains("0"))
+            throw new IllegalArgumentException();
+
+        // TODO : 사용자가 선택한 숫자를 List에 담아 리턴
+        List<Integer> numbers = new ArrayList<>();
+        for (char number : inputNumber.toCharArray())
+            numbers.add(Character.getNumericValue(number));
+
+        return numbers;
+    }
 
 }
