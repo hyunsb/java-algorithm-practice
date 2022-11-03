d5720bf79c4173b884c3dd9dcfb4be9154fb285f
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-04 04:05:13 +0900

    test

--- wooSecondWeek/Baseball.java
+++ wooSecondWeek/Baseball.java
@@ -7,12 +7,23 @@ public class Baseball {
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
         // TODO: 프로그램 구현
-        List<Integer> computer = baseball.selectRandomNumbers();
-        System.out.println(computer);
 
-        List<Integer> user = baseball.selectNumbers();
-        System.out.println(user);
+        boolean newGame = true; // 새로운 게임을 판별하는 변수
+        List<Integer> computer = new ArrayList<>();
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true){
+            if (newGame)
+                computer = baseball.selectRandomNumbers();
+
+            System.out.print("숫자를 입력해주세요 : ");
+            List<Integer> user = baseball.selectNumbers();
+
+
+            System.out.println(computer);
+            System.out.println(user);
+            break;
+        }
     }
 
     // TODO : 컴퓨터가 숫자를 랜덤으로 선택하여 리턴
@@ -32,7 +43,7 @@ public class Baseball {
         String inputNumber = Console.readLine();
 
         // TODO : 사용자가 잘못된 값을 입력할 시 IllegalArgumentException 발생
-        //isInputMatch(inputNumber);
+        isInputMatch(inputNumber);
 
         // TODO : 사용자가 선택한 숫자를 List에 담아 리턴
         List<Integer> numbers = new ArrayList<>();
@@ -49,6 +60,12 @@ public class Baseball {
                 || inputNumber.contains("0")
                 || !inputNumber.replaceAll("[1-9]", "").isEmpty())
             throw new IllegalArgumentException();
+    }
+
+    //TODO : 컴퓨터와 사용자의 수를 비교하여 힌트를 리턴
+    public String compareNumbers(){
+        String answer = "";
+        return answer;
     }
 
 }
