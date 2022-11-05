bd8bbdc407918af7866b1838c093bf5a6525eca6
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-05 19:48:28 +0900

    feat: 컴퓨터가 숫자를 선택하는 기능 추가 1에서 9까지의 서로다른 임의의 수 3개를 선택하여 저장한다.

--- wooSecondWeek/baseball.java
+++ wooSecondWeek/baseball.java
@@ -1,9 +1,29 @@
 package wooSecondWeek;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class baseball {
     final static int MIN_RANGE_NUM = 1;
     final static int MAX_RANGE_NUM = 9;
     final static int MAX_ARRAY_SIZE = 3;
 
+    public static void main(String[] args) {
 
+    }
+
+    // TODO : 컴퓨터가 1에서 9까지의 서로다른 임의의 수 3개를 선택하여 저장한다.
+    public List<Integer> SelectRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+
+        while (numbers.size() < MAX_ARRAY_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_RANGE_NUM, MAX_RANGE_NUM);
+            if (!numbers.contains(randomNumber)){
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
 }
