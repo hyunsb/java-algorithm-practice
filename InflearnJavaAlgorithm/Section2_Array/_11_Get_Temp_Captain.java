5698ba96ba8eb2c1a7c989f7e41faa86e6497916
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-07 19:12:07 +0900

    refactor: section 2-11 refactor 학생번호-1 로 출력되는 기능 수정 비교대상항목 비정상적 업데이트 수정

--- InflearnJavaAlgorithm/Section2_Array/_11_Get_Temp_Captain.java
+++ InflearnJavaAlgorithm/Section2_Array/_11_Get_Temp_Captain.java
@@ -8,26 +8,29 @@ public class _11_Get_Temp_Captain {
     public int solution(int length, int[][] studentInfo){
         int captain = 0;
         int maxClassmate = Integer.MIN_VALUE;
-        List<Integer> compareStudent = new ArrayList<>();
-
 
         for (int i=0; i<length; i++){
             int countClassmate = 0;
-            List<Integer> tempCompareList = compareStudent;
-            tempCompareList.add(i);
+            List<Integer> compareList = new ArrayList<>();
+            compareList.add(i);
+
             for (int j=0; j<5; j++){
                 for (int k=0; k<length; k++){
                     if (studentInfo[i][j] == studentInfo[k][j]
-                            && !tempCompareList.contains(k)){
+                            && !compareList.contains(k)){
                         countClassmate += 1;
-                        tempCompareList.add(k);
+                        compareList.add(k);
                     }
                 }
             }
-            if (maxClassmate < countClassmate) captain = i;
+
+            if (maxClassmate < countClassmate) {
+                captain = i;
+                maxClassmate = countClassmate;
+            }
         }
 
-        return captain;
+        return captain+1;
     }
 
     public static void main(String[] args) {
