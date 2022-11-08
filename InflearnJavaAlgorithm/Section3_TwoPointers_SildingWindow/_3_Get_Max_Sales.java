4eef9a477154310b5733f40359d81675da71819a
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-08 23:16:14 +0900

    feat(_3_Get_Max_Sales): section 3-3 solved 인프런 3-3 최대 매출

--- InflearnJavaAlgorithm/Section3_TwoPointers_SildingWindow/_3_Get_Max_Sales.java
+++ InflearnJavaAlgorithm/Section3_TwoPointers_SildingWindow/_3_Get_Max_Sales.java
@@ -17,7 +17,17 @@ public class _3_Get_Max_Sales {
         return maxSales;
     }
 
+    public int solution2(int n, int k, int[] sales){
+        int sale = 0;
+        for(int i=0; i<k; i++) sale += sales[i];
+        int maxSales = sale;
 
+        for(int i=k; i<n; i++){
+            sale = sale + sales[i] - sales[i-k];
+            maxSales = Math.max(maxSales, sale);
+        }
+        return maxSales;
+    }
 
     public static void main(String[] args) {
         _3_Get_Max_Sales T = new _3_Get_Max_Sales();
@@ -26,9 +36,10 @@ public class _3_Get_Max_Sales {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        int[] salses = new int[n];
-        for(int i=0; i<n; i++) salses[i] = sc.nextInt();
+        int[] sales = new int[n];
+        for(int i=0; i<n; i++) sales[i] = sc.nextInt();
 
-        System.out.println(T.solution(n, k, salses));
+        System.out.println(T.solution(n, k, sales));
+        System.out.println(T.solution2(n, k, sales));
     }
 }
