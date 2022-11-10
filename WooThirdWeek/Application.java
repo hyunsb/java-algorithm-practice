d628fa66dc1b960992a221e58de3a87d31da40b9
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-10 23:41:54 +0900

    feat: 당첨번호와 보너스 번호를 입력받는 기능 추가

--- WooThirdWeek/Application.java
+++ WooThirdWeek/Application.java
@@ -6,14 +6,7 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int tmp = IOValue.inputLottoAmount()/1000;
-        System.out.println();
-
-        List<Lotto> lottoList = new ArrayList<>();
-        while(lottoList.size() < tmp)
-            lottoList.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
-
-        for(Lotto ticket : lottoList)
-            System.out.println(ticket.getNumbers());
+        IOValue ioValue = new IOValue();
+        ioValue.buyTicket();
     }
 }
--- WooThirdWeek/IOValue.java
+++ WooThirdWeek/IOValue.java
@@ -1,5 +1,7 @@
 package WooThirdWeek;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class IOValue {
@@ -7,16 +9,27 @@ public class IOValue {
     private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
+    private static final String OUTPUT_LOTTO_PURCHASE_COUNT = "개를 구매했습니다.";
+
+    private static final String SEPARATED_CHARACTER = ",";
+
     private static final int TICKET_PRICE = 1000;
     private static final int MAX_VALUE_OF_LOTTO_NUMBER = 45;
     private static final int MIN_VALUE_OF_LOTTO_NUMBER = 1;
-
+    private static final int LOTTO_COUNT = 6;
 
     private static int lottoAmount;
     private static int ticketNumbers;
 
-    private static List<Lotto> LottoList;
+    private static final List<Lotto> LottoList = new ArrayList<>();
+    private static final List<Integer> winningNumber = new ArrayList<>();
 
+    public void buyTicket(){
+        initializationLottoAmount();
+        initializationTicketNumbers();
+        initializationLottoList();
+        printLottoNumbers();
+    }
 
     // TODO: 로또 금액 문장 출력 후 로또 금액을 입력 받는다.
     private static int inputLottoAmount(){
@@ -43,17 +56,21 @@ public class IOValue {
     // TODO: 로또 객체 생성 후 로또 번호를 랜덤으로 생성
     private static void createLottoTicket(){
         LottoList.add(new Lotto(Randoms.pickUniqueNumbersInRange(
-                        MIN_VALUE_OF_LOTTO_NUMBER, MAX_VALUE_OF_LOTTO_NUMBER, ticketNumbers)));
+                        MIN_VALUE_OF_LOTTO_NUMBER, MAX_VALUE_OF_LOTTO_NUMBER, LOTTO_COUNT)));
     }
 
-    // TODO: 로또 번호 출력
-    public static void printLottoNumbers(){
-        for(Lotto lottoticket : LottoList){
-
-        }
+    // TODO: 로또 구매 개수 및 번호 출력
+    public void printLottoNumbers(){
+        System.out.println("\n" + ticketNumbers + OUTPUT_LOTTO_PURCHASE_COUNT);
+        for(Lotto lottoTicket : LottoList)
+            System.out.println(lottoTicket.getNumbers());
     }
 
+    public void receiveWinningNumber(){
+        String winningNumbers = Console.readLine();
+        String[] winningNumberList = winningNumbers.split(SEPARATED_CHARACTER);
 
+    }
 
 
 }
