09c155edce4823c21aa9fb05e93b97fece8f3f1a
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-10 22:07:31 +0900

    test: 애플리케이션 동작 테스트

--- WooThirdWeek/Input.java
+++ WooThirdWeek/Input.java
@@ -1,12 +1,16 @@
 package WooThirdWeek;
 
-public class User {
+public class Input {
     private static final String INPUT_LOTTO_AMOUNT = "로또금액을 입력해 주세요.";
     private static final String INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    
+
     public static int inputLottoAmount(){
         System.out.println(INPUT_LOTTO_AMOUNT);
         return Error.isCorrectAmount(Console.readLine());
     }
+
+
+
+
 }
---
+++ WooThirdWeek/Output.java
@@ -0,0 +1,4 @@
+package WooThirdWeek;
+
+public class Output {
+}
