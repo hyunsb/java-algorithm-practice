44c453017f23fdd1c09bf55c3d094c1aee44c8ed
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-10 19:23:24 +0900

    feat(Error): 입력값이 1,000으로 나누어 떨어지지 않는 경우 예외 처리 기능 추가

--- WooThirdWeek/Error.java
+++ WooThirdWeek/Error.java
@@ -1,4 +1,13 @@
 package WooThirdWeek;
 
 public class Error {
+    final private static int TICKET_PRICE = 1000;
+
+    // TODO : 입력 값이 1,000으로 나누어 떨어지지 않는 경우 예외 처리 한다.
+    public boolean isDividedByTicketPrice(int purchaseAmount) throws IllegalArgumentException{
+        if (purchaseAmount % TICKET_PRICE > 0)
+            throw new IllegalArgumentException("[ERROR] The unit of the purchase amount is not 1,000");
+        return true;
+    }
+
 }
