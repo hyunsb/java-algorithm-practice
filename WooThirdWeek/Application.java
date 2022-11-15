970bf74573bd8f171c5f2afcb2334afd903ee33e
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-15 19:58:46 +0900

    feat: 오류 메시지 출력 기능 추가

--- WooThirdWeek/Application.java
+++ WooThirdWeek/Application.java
@@ -2,10 +2,13 @@ package WooThirdWeek;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         Domain domain = new Domain();
-        domain.buyTicket();
-        domain.receiveWinningNumber();
-        domain.printPrize();
+        try{
+            domain.buyTicket();
+            domain.receiveWinningNumber();
+            domain.printPrize();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
