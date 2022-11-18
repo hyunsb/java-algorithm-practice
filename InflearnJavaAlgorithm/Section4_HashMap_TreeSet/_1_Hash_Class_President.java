771f28fed78a3eace26394bbb5462d620e40560c
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-18 21:46:12 +0900

    feat(_1_Hash_Class_President): Update section 4-1 soltion code third way(메서드 분리 연습) 인프런 4-1 학급회장(Hash)

--- InflearnJavaAlgorithm/Section4_HashMap_TreeSet/_1_Hash_Class_President.java
+++ InflearnJavaAlgorithm/Section4_HashMap_TreeSet/_1_Hash_Class_President.java
@@ -40,6 +40,30 @@ public class _1_Hash_Class_President {
         return classPresident;
     }
 
+    public char solution3(int n, String voting){
+        return getClassPresident(voteCountInitialize(voting));
+    }
+
+    private HashMap<Character, Integer> voteCountInitialize(String voting){
+        HashMap<Character, Integer> voteCount = new HashMap<>();
+        for(char vote : voting.toCharArray())
+            voteCount.put(vote, voteCount.getOrDefault(vote, 0) + 1);
+        return voteCount;
+    }
+
+    private char getClassPresident(HashMap<Character, Integer> voteCount){
+        int maxValue = Integer.MIN_VALUE;
+        char classPresident = ' ';
+
+        for(char key : voteCount.keySet()){
+            if(voteCount.get(key) > maxValue) {
+                maxValue = voteCount.get(key);
+                classPresident = key;
+            }
+        }
+        return classPresident;
+    }
+
     public static void main(String[] args) {
         _1_Hash_Class_President T = new _1_Hash_Class_President();
         Scanner sc = new Scanner(System.in);
@@ -48,5 +72,6 @@ public class _1_Hash_Class_President {
         String voting = sc.next();
         System.out.println(T.solution(n, voting));
         System.out.println(T.solution2(n, voting));
+        System.out.println(T.solution3(n, voting));
     }
 }
