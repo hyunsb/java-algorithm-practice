fba04834b493ec05f530b91343a73a9e445c21da
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-18 23:32:44 +0900

    feat(_3_Hash_Type_Of_Sales): Update section 4-3 second solution code 인프런 4-3 매출액의 종류

--- InflearnJavaAlgorithm/Section4_HashMap_TreeSet/_3_Hash_Type_Of_Sales.java
+++ InflearnJavaAlgorithm/Section4_HashMap_TreeSet/_3_Hash_Type_Of_Sales.java
@@ -1,9 +1,6 @@
 package InflearnJavaAlgorithm.Section4_HashMap_TreeSet;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class _3_Hash_Type_Of_Sales {
     public static void main(String[] args) {
@@ -16,6 +13,7 @@ public class _3_Hash_Type_Of_Sales {
         for(int i=0; i<n; i++) sales[i] = sc.nextInt();
 
         for(int salesType : T.solution(n, k, sales)) System.out.print(salesType + " ");
+        for(int salesType : T.solution2(n, k, sales)) System.out.print(salesType + " ");
     }
 
     public List<Integer> solution(int n, int k, int[] sales){
@@ -29,6 +27,27 @@ public class _3_Hash_Type_Of_Sales {
             }
             lt++;
             answer.add(salesType.size());
+        }
+        return answer;
+    }
+
+    public List<Integer> solution2(int n, int k, int[] sales){
+        List<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> salesType = new HashMap<>();
+
+        for(int i=0; i<k-1; i++)
+            salesType.put(sales[i], salesType.getOrDefault(sales[i], 0) + 1);
+
+        int lt=0;
+        for(int rt=k-1; rt<n; rt++){
+            salesType.put(sales[rt], salesType.getOrDefault(sales[rt], 0) + 1);
+            answer.add(salesType.size());
+            salesType.put(sales[lt], salesType.get(sales[lt]) - 1);
+
+            if(salesType.get(sales[lt]) == 0)
+                salesType.remove(sales[lt]);
+
+            lt++;
         }
         return answer;
     }
