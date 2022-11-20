e6873532c367d2e46865a55a5ac6ff97711da611
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-21 01:02:03 +0900

    feat(_5_Kth_Number): Udate section 4-5 solution code 인프런 4-5 K번째 큰 수

---
+++ InflearnJavaAlgorithm/Section4_HashMap_TreeSet/_5_Kth_Number.java
@@ -0,0 +1,45 @@
+package InflearnJavaAlgorithm.Section4_HashMap_TreeSet;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class _5_Kth_Number {
+    // 인프런 4-5 K번째 큰 수
+    public static void main(String[] args) {
+        _5_Kth_Number T = new _5_Kth_Number();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] score = new int[n];
+        for(int i=0; i<n; i++) score[i] = sc.nextInt();
+        System.out.println(T.solution(n, k, score));
+    }
+
+    public int solution(int n, int k, int[] score){
+        TreeSet<Integer> scoreSet = new TreeSet<>();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i == j)
+                    continue;
+
+                for(int l=0; l<n; l++){
+                    if(j == l || i == l)
+                        continue;
+
+                    scoreSet.add(score[i] + score[j] + score[l]);
+                }
+            }
+        }
+
+        if(scoreSet.size() < k){
+            return -1;
+        }
+
+        for(int i=0; i<k-1; i++)
+            scoreSet.pollLast();
+
+        return scoreSet.last();
+    }
+}
