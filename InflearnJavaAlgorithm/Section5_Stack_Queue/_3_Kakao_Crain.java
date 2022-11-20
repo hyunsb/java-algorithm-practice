9b703f094b0db6ff7f00498576db4f764a000fd0
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-21 01:05:04 +0900

    feat(_3_Kakao_Crain): Udate section 5-3 solution code 인프런 5-3 크레인 인형뽑기 카카오

---
+++ InflearnJavaAlgorithm/Section5_Stack_Queue/_3_Kakao_Crain.java
@@ -0,0 +1,48 @@
+package InflearnJavaAlgorithm.Section5_Stack_Queue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class _3_Kakao_Crain {
+    //인프런 5-3 크레인 인형뽑기(카카오)
+    public static void main(String[] args) {
+        _3_Kakao_Crain T = new _3_Kakao_Crain();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = sc.nextInt();
+
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++) moves[i] = sc.nextInt();
+
+        System.out.println(T.solution(n, board, m, moves));
+    }
+
+
+
+    public int solution(int n, int[][] board, int m, int[] moves) {
+        Stack<Integer> basket = new Stack<>();
+        int answer = 0;
+        for (int move : moves) {
+            for (int j = 0; j < n; j++) {
+                if (board[j][move-1] != 0) {
+                    basket.push(board[j][move-1]);
+                    board[j][move-1] = 0;
+                    break;
+                }
+            }
+
+            int doll = basket.pop();
+            if(!basket.isEmpty() && doll == basket.peek()){
+                basket.pop();
+                answer += 2;
+                continue;
+            }
+            basket.push(doll);
+        }
+        return answer;
+    }
+}
