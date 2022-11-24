58ada0a81f30e254baa338af26d15b08ec78854e
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-24 13:23:15 +0900

    feat(_6_Queue_Saving_Princess): Update section 5-6 second solution code 인프런 5-6 공주 구하기

--- InflearnJavaAlgorithm/Section5_Stack_Queue/_6_Queue_Saving_Princess.java
+++ InflearnJavaAlgorithm/Section5_Stack_Queue/_6_Queue_Saving_Princess.java
@@ -11,6 +11,19 @@ public class _6_Queue_Saving_Princess {
         int n = sc.nextInt();
         int k = sc.nextInt();
         System.out.println(T.solution(n, k));
+        System.out.println(T.solution2(n, k));
+    }
+
+    public int solution2(int n, int k){
+        int answer = 0;
+        Queue<Integer> Q = new LinkedList<>();
+        for(int i=1; i<=n; i++) Q.offer(i);
+        while (!Q.isEmpty()){
+            for(int i=1; i<k; i++) Q.offer(Q.poll());
+            Q.poll();
+            if(Q.size() == 1) answer = Q.poll();
+        }
+        return answer;
     }
 
     public int solution(int n, int k){
