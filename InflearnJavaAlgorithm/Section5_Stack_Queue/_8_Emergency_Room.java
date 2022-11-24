90824474160ffc27c7c375f3512929561838ca23
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-24 14:05:02 +0900

    feat(_8_Emergency_Room): Update section 5-8 second solution code 인프런 5-8 응급실

--- InflearnJavaAlgorithm/Section5_Stack_Queue/_8_Emergency_Room.java
+++ InflearnJavaAlgorithm/Section5_Stack_Queue/_8_Emergency_Room.java
@@ -41,32 +41,27 @@ public class _8_Emergency_Room {
 
     public int solution(int n, int m, int[] patients){
         Queue<Person> patientsList = new LinkedList<>();
+        int answer = 1;
         for(int i=0; i<n; i++)
             patientsList.offer(new Person(i, patients[i]));
 
-        int count = 0, id = -1;
-        while (id!=m){
-            Person patient = patientsList.peek();
-
-            assert patientsList.peek() != null;
-            int patientId = patientsList.peek().id;
-
-            for(Person tmp : patientsList){
-                if (patient.priority < tmp.priority) {
-                    patientId = tmp.id;
+        while (!patientsList.isEmpty()){
+            Person patient = patientsList.poll();
+            for(Person x : patientsList){
+                if(patient.priority < x.priority){
+                    patientsList.offer(patient);
+                    patient = null;
                     break;
                 }
             }
 
-            if(patient.id == patientId){
-                patientsList.remove();
-                id = patientId;
-                count += 1;
-            }
+            if(patient != null)
+                if(patient.id == m)
+                    return answer;
+                else answer++;
         }
 
-        return count;
-
+        return answer;
     }
 
     public class Person {
