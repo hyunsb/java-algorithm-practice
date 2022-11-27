74b6351b5f207445c6ecdf6909f50e94796312ea
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-27 23:23:35 +0900

    feat(_5_Check_For_Duplicates): Update section 6-5 solution code (Use Sorting) 인프런 6-5 중복 확인

--- InflearnJavaAlgorithm/Section6_Sorting_And_Searching/_5_Check_For_Duplicates.java
+++ InflearnJavaAlgorithm/Section6_Sorting_And_Searching/_5_Check_For_Duplicates.java
@@ -1,9 +1,6 @@
 package InflearnJavaAlgorithm.Section6_Sorting_And_Searching;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class _5_Check_For_Duplicates {
     public static void main(String[] args) {
@@ -35,6 +32,16 @@ public class _5_Check_For_Duplicates {
         for(int i : stud){
             if(map.getOrDefault(i, 0) != 0) return 'D';
             map.put(i, 1);
+        }
+
+        return answer;
+    }
+
+    public char solution3(int n, int[] stud){
+        char answer = 'U';
+        Arrays.sort(stud);
+        for(int i=0; i<n-1; i++){
+            if(stud[i] == stud[i+1]) return 'D';
         }
 
         return answer;
