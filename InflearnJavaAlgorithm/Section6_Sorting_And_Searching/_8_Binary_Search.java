f0dfe563e4cd3bda6693b6b7416c56744ceceda5
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-28 15:00:24 +0900

    feat: Update section 6-8 solution code (_8_Binary_Search) 인프런 6-8 이분검색

--- InflearnJavaAlgorithm/Section6_Sorting_And_Searching/_8_Binary_Search.java
+++ InflearnJavaAlgorithm/Section6_Sorting_And_Searching/_8_Binary_Search.java
@@ -29,4 +29,24 @@ public class _8_Binary_Search {
         }
 
     }
+
+    public int solution2(int n, int m, int[] num){
+        int answer = 0;
+
+        Arrays.sort(num);
+        int lt = 0, rt = num.length-1;
+        while (lt <= rt){
+            int mid = (lt + rt) / 2;
+
+            if(num[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+
+            if(num[mid] > m) rt = mid - 1;
+            else lt = mid + 1;
+        }
+
+        return answer;
+    }
 }
