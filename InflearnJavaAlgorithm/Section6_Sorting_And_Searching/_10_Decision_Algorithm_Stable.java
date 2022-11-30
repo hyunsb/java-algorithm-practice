8d57c187da0d77315accb0186570cd0f9d42a564
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-30 21:47:53 +0900

    Update section 6-10 solution code (_10_Decision_Algorithm_Stable) 인프런 6-10 마구간 정하기(결정알고리즘)

--- InflearnJavaAlgorithm/Section6_Sorting_And_Searching/_10_Decision_Algorithm_Stable.java
+++ InflearnJavaAlgorithm/Section6_Sorting_And_Searching/_10_Decision_Algorithm_Stable.java
@@ -1,8 +1,47 @@
 package InflearnJavaAlgorithm.Section6_Sorting_And_Searching;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class _10_Decision_Algorithm_Stable {
     public static void main(String[] args) {
+        _10_Decision_Algorithm_Stable T = new _10_Decision_Algorithm_Stable();
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        int[] xi = new int[n];
+        for(int i=0; i<n; i++) xi[i] = sc.nextInt();
+        System.out.println(T.solution(n, c, xi));
     }
-    
+
+    public int solution(int n, int c, int[] xi){
+        int answer = 0;
+        Arrays.sort(xi);
+        int lt = 1;
+        int rt = xi[xi.length-1];
+
+        while (lt <= rt){
+            int mid = (lt + rt) / 2;
+            if (countingHorse(xi, mid) >= c) {
+                answer = mid;
+                lt = mid + 1;
+            }else {
+                rt = mid - 1;
+            }
+
+        }
+        return answer;
+    }
+
+    public int countingHorse(int[] stableCoordinates, int maxDistance){
+        int endPoint = stableCoordinates[0], horseCount = 1;
+        for(int i=1; i<stableCoordinates.length; i++){
+            if(stableCoordinates[i] - endPoint >= maxDistance){
+                endPoint = stableCoordinates[i];
+                horseCount++;
+            }
+        }
+        return horseCount;
+    }
 }
