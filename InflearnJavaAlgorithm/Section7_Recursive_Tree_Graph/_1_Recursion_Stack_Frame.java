50794aa683b98f06d2e819aa5a167007fbb74cc7
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-02 17:27:20 +0900

    Update section 7-1 solution code (_1_Recursion_Stack_Frame) 인프런 7-1 재귀함수(스택프레임)

--- InflearnJavaAlgorithm/Section7_Recursive_Tree_Graph/_1_Recursion_Stack_Frame.java
+++ InflearnJavaAlgorithm/Section7_Recursive_Tree_Graph/_1_Recursion_Stack_Frame.java
@@ -8,6 +8,10 @@ public class _1_Recursion_Stack_Frame {
      *
      * 재귀함수? 백트래킹? 스택프레임?
      * 재귀함수(Recursion): 자기가 자기자신을 호출하는 함수
+     *
+     * 재귀함수는 스택프레임을 사용한다. (모든 함수는 실행 시 스택에 저장되며 스택프레임이 생성된다.)
+     * 스택프레임은 매개변수 주소, 지역변수 주소, 복귀 주소를 가진다.
+     *
      * */
     public static void main(String[] args) {
         _1_Recursion_Stack_Frame T = new _1_Recursion_Stack_Frame();
