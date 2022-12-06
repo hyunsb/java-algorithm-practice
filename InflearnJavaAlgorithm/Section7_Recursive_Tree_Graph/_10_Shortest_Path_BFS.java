09dc5bcbe134944710358c43b6a5d7999afb3f7b
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-06 17:26:04 +0900

    Update section 7-10 solution code (_10_Shortest_Path_BFS) 인프런 7-10 Tree 말단노드까지의 가장 짧은 경로(BFS)

---
+++ InflearnJavaAlgorithm/Section7_Recursive_Tree_Graph/_10_Shortest_Path_BFS.java
@@ -0,0 +1,51 @@
+package InflearnJavaAlgorithm.Section7_Recursive_Tree_Graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Node_10 {
+    int data;
+    Node_10 lt, rt;
+    Node_10(int data){
+        this.data = data;
+        lt = null;
+        rt = null;
+    }
+}
+
+public class _10_Shortest_Path_BFS {
+    Node_10 root;
+
+    public static void main(String[] args) {
+        _10_Shortest_Path_BFS tree = new _10_Shortest_Path_BFS();
+        Scanner sc = new Scanner(System.in);
+        tree.root = new Node_10(1);
+        tree.root.lt = new Node_10(2);
+        tree.root.rt = new Node_10(3);
+        tree.root.lt.lt = new Node_10(4);
+        tree.root.lt.rt = new Node_10(5);
+        System.out.println(tree.BFS(tree.root));
+    }
+
+    public int BFS(Node_10 root){
+        Queue<Node_10> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+
+        while (!queue.isEmpty()){
+            for(int i=0; i<queue.size(); i++){
+                Node_10 currentNode = queue.poll();
+
+                if(currentNode.lt == null && currentNode.rt == null)
+                    return level;
+                if(currentNode.lt != null)
+                    queue.offer(currentNode.lt);
+                if(currentNode.rt != null)
+                    queue.offer(currentNode.rt);
+            }
+            level++;
+        }
+        return 0;
+    }
+}
