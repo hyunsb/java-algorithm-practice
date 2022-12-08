9cc25baf2401bb02bd9f19d9ac3ab36d4d9b210a
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-08 22:16:42 +0900

    Update section 8-1 solution code (_1_Subset_With_Equal_Sum_DFS) 인프런 8-1 합이 같은 부분집합

--- InflearnJavaAlgorithm/Section8_DFS_BFS/_1_Subset_With_Equal_Sum_DFS.java
+++ InflearnJavaAlgorithm/Section8_DFS_BFS/_1_Subset_With_Equal_Sum_DFS.java
@@ -7,19 +7,38 @@ public class _1_Subset_With_Equal_Sum_DFS {
     static int n, total = 0;
     boolean flag = false;
 
-    public void DFS(int L, int sum, int[] arr){
+    public void DFS_(int idx, int sum, int[] arr){
         if(flag) return;
         if(sum > total/2) return;
-        if(L==n){
-            if( (total-sum) == sum ){
+
+        if(idx == n){
+            // 부분집합 생성을 완료한 경우의 수
+            if(sum == (total - sum)) {
                 answer = "YES";
                 flag = true;
             }
         }else {
-            DFS(L+1, sum+arr[L], arr);
-            DFS(L+1, sum, arr);
+            // 다음 숫자를 포함하여 부분집합을 생성하는 경우의 수
+            DFS_(idx+1, sum + arr[idx], arr);
+            // 다음 숫자를 포함하지 않고 부분집합을 생성하는 경우의 수
+            DFS_(idx+1, sum, arr);
         }
     }
+
+
+//    public void DFS(int L, int sum, int[] arr){
+//        if(flag) return;
+//        if(sum > total/2) return;
+//        if(L==n){
+//            if( (total-sum) == sum ){
+//                answer = "YES";
+//                flag = true;
+//            }
+//        }else {
+//            DFS(L+1, sum+arr[L], arr);
+//            DFS(L+1, sum, arr);
+//        }
+//    }
 
     public static void main(String[] args) {
         _1_Subset_With_Equal_Sum_DFS T = new _1_Subset_With_Equal_Sum_DFS();
@@ -32,7 +51,8 @@ public class _1_Subset_With_Equal_Sum_DFS {
             total += arr[i];
         }
 
-        T.DFS(0, 0, arr);
+//        T.DFS(0, 0, arr);
+        T.DFS_(0, 0, arr);
         System.out.println(answer);
     }
 }
