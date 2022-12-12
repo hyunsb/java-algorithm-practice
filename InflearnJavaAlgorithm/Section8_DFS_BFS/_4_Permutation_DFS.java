9dd1c80c2cd14d537e97a21aa313d907c8e200fd
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-12 21:39:12 +0900

    Style: Update section 8-4 solution code (_4_Permutation_DFS) 인프런 8-4 중복순열

--- InflearnJavaAlgorithm/Section8_DFS_BFS/_4_Permutation_DFS.java
+++ InflearnJavaAlgorithm/Section8_DFS_BFS/_4_Permutation_DFS.java
@@ -8,7 +8,8 @@ public class _4_Permutation_DFS{
 
 	public void DFS(int level){
 		if(level == m){
-			for(int n : pm) System.out.print(n + " ");
+			for(int n : pm)
+				System.out.print(n + " ");
 			System.out.println();
 		}else {
 			for(int i=1; i<=n; i++){
@@ -16,15 +17,14 @@ public class _4_Permutation_DFS{
 				DFS(level+1);
 			}
 		}
-
 	}
 
 	public static void main(String[] args){
 		_4_Permutation_DFS T = new _4_Permutation_DFS();
-		Scanner kb = new Scanner(System.in);
-		n=kb.nextInt();
-		m=kb.nextInt();
-		pm=new int[m];
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		pm = new int[m];
 		T.DFS(0);
 	}
 }
