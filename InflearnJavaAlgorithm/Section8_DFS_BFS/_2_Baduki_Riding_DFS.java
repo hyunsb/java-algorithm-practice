9b68c316c63b2ef4c7cab836ce764a17d8442e51
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-12 22:03:58 +0900

    Update section 8-2 solution code (_2_Baduki_Riding_DFS) 인프런 8-2 바둑이 승차(DFS)

--- InflearnJavaAlgorithm/Section8_DFS_BFS/_2_Baduki_Riding_DFS.java
+++ InflearnJavaAlgorithm/Section8_DFS_BFS/_2_Baduki_Riding_DFS.java
@@ -6,9 +6,10 @@ public class _2_Baduki_Riding_DFS {
     static int n, maxWeight, totalWeight = 0;
 
     public void DFS(int idx, int sum, int[] arr){
-        if(idx == n || ((sum + arr[idx]) > maxWeight)) {
+        if(sum > maxWeight) return;
+
+        if(idx == n) {
             totalWeight = Math.max(totalWeight, sum);
-            return;
         } else {
             DFS(idx+1, (sum + arr[idx]), arr);
             DFS(idx+1, sum, arr);
