7847a05948cded8eb33f123ddf23956b53897a50
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-12 23:09:34 +0900

    Update section 8-6 solution code (_6_Permutation_DFS) 인프런 8-6 순열 구하기

--- InflearnJavaAlgorithm/Section8_DFS_BFS/_6_Permutation_DFS.java
+++ InflearnJavaAlgorithm/Section8_DFS_BFS/_6_Permutation_DFS.java
@@ -34,3 +34,41 @@ public class _6_Permutation_DFS {
         T.DFS(0);
     }
 }
+
+class _6_Permutation_DFS_Inflearn {
+    static int[] pm, ch, arr;
+    static int n, m;
+
+    public void DFS(int L){
+        if(L == m) {
+            for(int x : pm) System.out.print(x + " ");
+            System.out.println();
+        }else {
+            for(int i=0; i<n; i++){
+                if(ch[i] == 0){
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    DFS(L+1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        _6_Permutation_DFS_Inflearn T = new _6_Permutation_DFS_Inflearn();
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        arr = new int[n];
+        for(int i=0; i<n; i++)
+            arr[i] = sc.nextInt();
+
+        ch = new int[n];
+        pm = new int[m];
+
+        T.DFS(0);
+    }
+}
