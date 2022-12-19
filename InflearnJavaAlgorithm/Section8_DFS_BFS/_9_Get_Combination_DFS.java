7d0f5373051eb47c07085948af87e192915789ea
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-19 18:36:05 +0900

    Update section 8-9 solution code (_9_Get_Combination_DFS_Inflearn) 인프런 8-9 조합 구하기

--- InflearnJavaAlgorithm/Section8_DFS_BFS/_9_Get_Combination_DFS.java
+++ InflearnJavaAlgorithm/Section8_DFS_BFS/_9_Get_Combination_DFS.java
@@ -2,7 +2,34 @@ package InflearnJavaAlgorithm.Section8_DFS_BFS;
 import java.util.*;
 
 public class _9_Get_Combination_DFS {
+    Scanner sc = new Scanner(System.in);
+    int n, m;
+    int[] combi;
 
+    _9_Get_Combination_DFS(){
+        n = sc.nextInt();
+        m = sc.nextInt();
+        combi = new int[m];
+
+        DFS(0, 1);
+    }
+
+    public void DFS(int level, int number){
+        if (level == m) {
+            for(int x : combi)
+                System.out.print(x + " ");
+            System.out.println();
+        }else {
+            for(int i=number; i<=n; i++){
+                combi[level] = i;
+                DFS(level+1, i+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        _9_Get_Combination_DFS main = new _9_Get_Combination_DFS();
+    }
 }
 
 class _9_Get_Combination_DFS_Inflearn {
