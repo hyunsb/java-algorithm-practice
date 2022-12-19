0f3ce7f7e7832210aa787cf1e84937c2ddcd9496
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-19 18:15:05 +0900

    Update section 8-8 solution code (_8_Sequence_Conjecture_DFS) 인프런 8-8 수열 추측하기

--- InflearnJavaAlgorithm/Section8_DFS_BFS/_8_Sequence_Conjecture_DFS.java
+++ InflearnJavaAlgorithm/Section8_DFS_BFS/_8_Sequence_Conjecture_DFS.java
@@ -3,8 +3,62 @@ package InflearnJavaAlgorithm.Section8_DFS_BFS;
 import java.util.Scanner;
 
 public class _8_Sequence_Conjecture_DFS {
-    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    static int[][] dy;
+    static int[] b, p, ch;
+    static int n, f;
 
+    static boolean flag = false;
+
+    _8_Sequence_Conjecture_DFS(){
+        n = sc.nextInt();
+        f = sc.nextInt();
+
+        dy = new int[n][n];
+        b = new int[n];
+        p = new int[n];
+        ch = new int[n+1];
+
+        for(int i=0; i<n; i++)
+            b[i] = combi(n-1, i);
+
+        DFS(0, 0);
+    }
+
+    public void DFS(int L, int sum){
+        if(flag)
+            return;
+
+        if(L == n){
+            if(sum == f) {
+                for(int x : p)
+                    System.out.print(x + " ");
+                flag = true;
+            }
+        }else {
+            for(int i=1; i<=n; i++){
+                if(ch[i] == 0){
+                    ch[i] = 1;
+                    p[L] = i;
+                    DFS(L+1, sum + (p[L] * b[L]));
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public int combi(int n, int r){
+        if(dy[n][r] > 0)
+            return dy[n][r];
+
+        if(n==r || r==0)
+            return 1;
+        else
+            return dy[n][r] = combi(n-1, r-1) + combi(n-1, r);
+    }
+
+    public static void main(String[] args) {
+        _8_Sequence_Conjecture_DFS T = new _8_Sequence_Conjecture_DFS();
     }
 }
 
@@ -57,6 +111,4 @@ class _8_Sequence_Conjecture_DFS_Inflearn {
 
         T.DFS(0, 0);
     }
-
-
 }
