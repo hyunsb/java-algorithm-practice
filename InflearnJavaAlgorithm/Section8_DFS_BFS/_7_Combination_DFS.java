1bdba562beeff1b54cbf298585c46ee50061b662
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-21 22:01:33 +0900

    Update section 8-7 solution code (_7_Combination_DFS) 인프런 8-7 조합수(메모이제이션)

--- InflearnJavaAlgorithm/Section8_DFS_BFS/_7_Combination_DFS.java
+++ InflearnJavaAlgorithm/Section8_DFS_BFS/_7_Combination_DFS.java
@@ -40,3 +40,19 @@ class _7_Combination_DFS_Infleran{
         System.out.println(T.DFS(n, r));
     }
 }
+
+class _7_Combination_DFS_Practice{
+    int[][] memoization = new int[34][34];
+
+    int DFS(int n, int r){
+        if(memoization[n][r] != 0) return memoization[n][r];
+        if(n == r || r == 0) return 1;
+        return DFS(n-1, r-1) + DFS(n-1, r);
+    }
+
+    public static void main(String[] args) {
+        _7_Combination_DFS_Infleran T = new _7_Combination_DFS_Infleran();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(T.DFS(sc.nextInt(), sc.nextInt()));
+    }
+}
