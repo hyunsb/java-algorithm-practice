27d86b41b6967b2ef9029336317c22f8e3642357
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-21 21:16:58 +0900

    Update section 8-4 solution code (_4_Permutation_With_Repetition_DFS) 인프런 8-4 중복순열

--- InflearnJavaAlgorithm/Section8_DFS_BFS/_4_Permutation_With_Repetition_DFS.java
+++ InflearnJavaAlgorithm/Section8_DFS_BFS/_4_Permutation_With_Repetition_DFS.java
@@ -2,12 +2,40 @@ package InflearnJavaAlgorithm.Section8_DFS_BFS;
 
 import java.util.*;
 
-public class _4_Permutation_With_Repetition_DFS {
+/**
+ * 중복순열 구하기
+ * </br>
+ * 1부터 N까지 번호가 적힌 구슬이 있습니다. 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열
+ * 하는 방법을 모두 출력합니다.
+ * */
 
+public class _4_Permutation_With_Repetition_DFS {
+	static int n, m;
+	static int[] permutation;
+
+	void DFS(int level){
+		if(level == m){
+			for (int num : permutation)
+				System.out.print(num + " ");
+			System.out.println();
+			return;
+		}
+
+		for(int i=1; i<=n; i++){
+			permutation[level] = i;
+			DFS(level+1);
+		}
+	}
 
 	public static void main(String[] args){
 		_4_Permutation_With_Repetition_DFS T = new _4_Permutation_With_Repetition_DFS();
 		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		m = sc.nextInt();
+		permutation = new int[m];
+
+		T.DFS(0);
 	}
 }
 
