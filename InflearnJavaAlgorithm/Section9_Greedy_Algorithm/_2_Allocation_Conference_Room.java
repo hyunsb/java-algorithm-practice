1f16b0873c2384a5faf372bf910c84cabdf689d1
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-26 21:20:01 +0900

    Update section 9-2 solution code (_2_Allocation_Conference_Room) 인프런 9-2 회의실 배정

--- InflearnJavaAlgorithm/Section9_Greedy_Algorithm/_2_Allocation_Conference_Room.java
+++ InflearnJavaAlgorithm/Section9_Greedy_Algorithm/_2_Allocation_Conference_Room.java
@@ -56,3 +56,46 @@ public class _2_Allocation_Conference_Room {
         System.out.println(main.solution(conferenceList));
     }
 }
+
+//============================Inflearn Code=====================================//
+
+class Time implements Comparable<Time>{
+    public int s, e;
+    public Time(int s, int e) {
+        this.s = s;
+        this.e = e;
+    }
+
+    @Override
+    public int compareTo(Time o) {
+        if(this.e == o.e) return this.s - o.s;
+        else return this.e - o.e;
+    }
+}
+class _2_Allocation_Conference_Room_Inflearn{
+    public int solution(ArrayList<Time> arr, int n){
+        int cnt = 0;
+        Collections.sort(arr);
+        int et=0;
+        for(Time ob : arr){
+            if(ob.s >= et){
+                cnt++;
+                et = ob.e;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        _2_Allocation_Conference_Room_Inflearn T = new _2_Allocation_Conference_Room_Inflearn();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Time> arr = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+            arr.add(new Time(s, e));
+        }
+        System.out.println(T.solution(arr, n));
+    }
+}
