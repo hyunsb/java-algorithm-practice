19b6852351adaf004a96f375e910f6b87bc51350
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-26 20:39:40 +0900

    Update section 9-1 solution code (_1_A_Sumo_Wrestler) 인프런 9-1 씨름 선수

--- InflearnJavaAlgorithm/Section9_Greedy_Algorithm/_1_A_Sumo_Wrestler.java
+++ InflearnJavaAlgorithm/Section9_Greedy_Algorithm/_1_A_Sumo_Wrestler.java
@@ -62,3 +62,47 @@ public class _1_A_Sumo_Wrestler {
         System.out.println(answer);
     }
 }
+
+class Body implements Comparable<Body>{
+    public int h, w;
+    Body(int h, int w){
+        this.h = h;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Body o) {
+        return o.h - this.h;
+    }
+}
+
+class _1_A_Sumo_Wrestler_Inflearn{
+    public int solution(ArrayList<Body> arr, int n){
+        int cnt = 0;
+        Collections.sort(arr);
+        int max = Integer.MIN_VALUE;
+
+        for(Body ob : arr){
+            if(ob.w > max){
+                max = ob.w;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        _1_A_Sumo_Wrestler_Inflearn T = new _1_A_Sumo_Wrestler_Inflearn();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ArrayList<Body> arr = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            arr.add(new Body(h, w));
+        }
+        System.out.println(T.solution(arr, n));
+    }
+}
