a07aba995cf35bbb78e6b47cf712fbfbb47cdc09
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-27 21:58:15 +0900

    Update section 9-5 solution code (_5_Dijkstra_Algorithm) 인프런 9-5 다익스트라 알고리즘

--- InflearnJavaAlgorithm/Section9_Greedy_Algorithm/_5_Dijkstra_Algorithm.java
+++ InflearnJavaAlgorithm/Section9_Greedy_Algorithm/_5_Dijkstra_Algorithm.java
@@ -74,6 +74,54 @@ public class _5_Dijkstra_Algorithm {
         }
     }
 }
+//======================Inflearn Code============================//
+class _5_Dijkstra_Algorithm_Infelarn{
+    static int n, m;
+    static ArrayList<ArrayList<Edge>> graph;
+    static int[] dis;
+
+    public void solution(int v){
+        PriorityQueue<Edge> pQ = new PriorityQueue<>();
+        pQ.offer(new Edge(v, 0));
+        dis[v]=0;
+        while(!pQ.isEmpty()){
+            Edge tmp=pQ.poll();
+            int now=tmp.vertex;
+            int nowCost=tmp.cost;
+            if(nowCost>dis[now]) continue;
+            for(Edge ob : graph.get(now)){
+                if(dis[ob.vertex]>nowCost+ob.cost){
+                    dis[ob.vertex]=nowCost+ob.cost;
+                    pQ.offer(new Edge(ob.vertex, nowCost+ob.cost));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        _5_Dijkstra_Algorithm_Infelarn T = new _5_Dijkstra_Algorithm_Infelarn();
+        Scanner kb = new Scanner(System.in);
+        n=kb.nextInt();
+        m=kb.nextInt();
+        graph = new ArrayList<ArrayList<Edge>>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<Edge>());
+        }
+        dis=new int[n+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        for(int i=0; i<m; i++){
+            int a=kb.nextInt();
+            int b=kb.nextInt();
+            int c=kb.nextInt();
+            graph.get(a).add(new Edge(b, c));
+        }
+        T.solution(1);
+        for(int i=2; i<=n; i++){
+            if(dis[i]!=Integer.MAX_VALUE) System.out.println(i+" : "+dis[i]);
+            else System.out.println(i+" : impossible");
+        }
+    }
+}
 
 
 //======================Practice Code===========================//
