cac06435a548fb05939287c8bf60a8b220c54a74
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-27 16:01:10 +0900

    Update section 9-3 solution code (_3_Wedding) 인프런 9-3 결혼식

--- InflearnJavaAlgorithm/Section9_Greedy_Algorithm/_3_Wedding.java
+++ InflearnJavaAlgorithm/Section9_Greedy_Algorithm/_3_Wedding.java
@@ -2,7 +2,6 @@ package Section9_Greedy_Algorithm;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -74,29 +73,30 @@ public class _3_Wedding {
 
 
 //==========================Inflearn Code===================================//
-class Time implements Comparable<Time>{
+
+class WeddingTime implements Comparable<WeddingTime>{
     public int time;
     public char state;
 
-    Time(int time, char state){
+    WeddingTime(int time, char state){
         this.time = time;
         this.state = state;
     }
 
     @Override
-    public int compareTo(Time o) {
+    public int compareTo(WeddingTime o) {
         if(this.time == o.time)
             return this.state - o.state;
         else return this.time - o.time;
     }
 }
 class _3_Wedding_Inflearn{
-    public int solution(ArrayList<Time> arr){
+    public int solution(ArrayList<WeddingTime> arr){
         int answer = Integer.MIN_VALUE;
         Collections.sort(arr);
         int cnt = 0;
 
-        for(Time ob : arr){
+        for(WeddingTime ob : arr){
             if(ob.state == 's') cnt++;
             else cnt--;
             answer = Math.max(answer, cnt);
@@ -109,12 +109,12 @@ class _3_Wedding_Inflearn{
         _3_Wedding_Inflearn T = new _3_Wedding_Inflearn();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        ArrayList<Time> arr = new ArrayList<>();
+        ArrayList<WeddingTime> arr = new ArrayList<>();
         for(int i=0; i<n; i++){
             int sT = sc.nextInt();
             int eT = sc.nextInt();
-            arr.add(new Time(sT, 's'));
-            arr.add(new Time(eT, 'e'));
+            arr.add(new WeddingTime(sT, 's'));
+            arr.add(new WeddingTime(eT, 'e'));
         }
         System.out.println(T.solution(arr));
     }
