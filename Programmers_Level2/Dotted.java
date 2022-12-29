ec61f4f2f4ff5aa4576c3b05f87ca369bf2250b1
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-29 17:30:28 +0900

    Update 프로그래머스 점찍기 Solution Code

---
+++ Programmers_Level2/Dotted.java
@@ -0,0 +1,24 @@
+package Level2;
+
+import java.util.Scanner;
+
+public class Dotted {
+
+    public long solution(int k, int d) {
+        long answer = 0;
+        for(int x=0; x<=d; x+=k){
+            int maxY = (int)Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2));
+
+            answer += maxY/k + 1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Dotted main = new Dotted();
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int d= sc.nextInt();
+        System.out.println(main.solution(k, d));
+    }
+}
