b1e1012d0ffaa41c18d97e2f4945907fd24745b0
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-29 22:49:14 +0900

    Update 프로그래머스 택배상자 Solution Code

--- Programmers_Level2/Delivery_Box.java
+++ Programmers_Level2/Delivery_Box.java
@@ -5,8 +5,30 @@ import java.util.Stack;
 public class Delivery_Box {
     public int solution(int[] order) {
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> saveOrder = new Stack<>();
 
+        int defaultOrder = 1;
+        int nowOrder = 0;
+
+        while (true) {
+            System.out.println(order[nowOrder] + " " + defaultOrder + " : " + saveOrder);
+            if(!saveOrder.isEmpty() && saveOrder.peek() == order[nowOrder]){
+                answer++;
+                saveOrder.pop();
+                nowOrder++;
+                continue;
+            }
+
+            if(defaultOrder > order.length) break;
+
+            if(defaultOrder == order[nowOrder]){
+                answer++;
+                nowOrder++;
+            }else {
+                saveOrder.push(defaultOrder);
+            }
+            defaultOrder++;
+        }
 
         return answer;
     }
