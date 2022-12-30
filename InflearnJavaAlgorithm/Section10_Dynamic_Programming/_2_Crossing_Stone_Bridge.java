5e258161c2d452d19a83ebeaab1b74ba51977b89
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-30 18:08:27 +0900

    Update section 10-2 solution code (_2_Crossing_Stone_Bridge) 인프런 10-2 돌다리 건너기

--- InflearnJavaAlgorithm/Section10_Dynamic_Programming/_2_Crossing_Stone_Bridge.java
+++ InflearnJavaAlgorithm/Section10_Dynamic_Programming/_2_Crossing_Stone_Bridge.java
@@ -39,7 +39,20 @@ public class _2_Crossing_Stone_Bridge {
 //=====================Inflearn Code====================//
 
 class _2_Crossing_Stone_Bridge_Inflearn{
+    static int[] dy;
+    public int solution(int n){
+        dy[1] = 1;
+        dy[2] = 2;
+        for(int i=3; i<=n+1; i++){
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+        return dy[n+1];
+    }
     public static void main(String[] args) {
-
+        _2_Crossing_Stone_Bridge_Inflearn T = new _2_Crossing_Stone_Bridge_Inflearn();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dy = new int[n+2];
+        System.out.println(T.solution(n));
     }
 }
