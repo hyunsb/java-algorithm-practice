a445306b0fe4be98d243c3aca8620f297eafd3b6
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-12-30 21:11:15 +0900

    Update section 10-3 solution code (_3_Maximum_Partial_Increment_Sequence) 인프런 10-3 최대 부분 증가 수열

--- InflearnJavaAlgorithm/Section10_Dynamic_Programming/_3_Maximum_Partial_Increment_Sequence.java
+++ InflearnJavaAlgorithm/Section10_Dynamic_Programming/_3_Maximum_Partial_Increment_Sequence.java
@@ -1,7 +1,6 @@
 package Section10_Dynamic_Programming;
 
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -56,8 +55,34 @@ public class _3_Maximum_Partial_Increment_Sequence {
 
 //==================Inflearn Code===================//
 class _3_Maximum_Partial_Increment_Sequence_Inflearn{
-    static int[] arr, dy;
+    static int[] dy;
+
+    public int solution(int[] arr){
+        int answer = 0;
+        dy = new int[arr.length];
+        dy[0] = 1;
+        for(int i=1; i<arr.length; i++){
+            int max = 0;
+            for(int j=i-1; j>=0; j--){
+                if(arr[j] < arr[i] && dy[j] > max)
+                    max = dy[j];
+            }
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-        
+        _3_Maximum_Partial_Increment_Sequence_Inflearn T = new _3_Maximum_Partial_Increment_Sequence_Inflearn();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.println(T.solution(arr));
     }
 }
