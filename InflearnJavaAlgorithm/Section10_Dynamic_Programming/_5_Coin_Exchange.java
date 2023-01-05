cffc16612e4785e980ab160c9bdacc0f84f5658e
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-05 19:29:35 +0900

    Update section 10-5 solution code (_5_Coin_Exchange) 인프런 10-5 동전교환

--- InflearnJavaAlgorithm/Section10_Dynamic_Programming/_5_Coin_Exchange.java
+++ InflearnJavaAlgorithm/Section10_Dynamic_Programming/_5_Coin_Exchange.java
@@ -28,3 +28,33 @@ public class _5_Coin_Exchange {
         System.out.print(T.solution(arr));
     }
 }
+
+
+
+class _5_Coin_Exchange_Inflearn{
+    static int n, m;
+    static int[] dy;
+    public int solution(int[] coin){
+        Arrays.fill(dy, Integer.MAX_VALUE);
+        dy[0]=0;
+        for(int i=0; i<n; i++){
+            for(int j=coin[i]; j<=m; j++){
+                dy[j]=Math.min(dy[j], dy[j-coin[i]]+1);
+            }
+        }
+        return dy[m];
+    }
+
+    public static void main(String[] args){
+        _5_Coin_Exchange_Inflearn T = new _5_Coin_Exchange_Inflearn();
+        Scanner kb = new Scanner(System.in);
+        n=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+        m=kb.nextInt();
+        dy=new int[m+1];
+        System.out.print(T.solution(arr));
+    }
+}
