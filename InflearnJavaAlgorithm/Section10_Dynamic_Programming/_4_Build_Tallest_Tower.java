e84ac243b50ef39afe60bd345810a749858469a9
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-01-06 16:23:05 +0900

    Update section 10-4 solution code (_4_Build_Tallest_Tower) 인프런 10-4 가장 높은 탑 쌓기

--- InflearnJavaAlgorithm/Section10_Dynamic_Programming/_4_Build_Tallest_Tower.java
+++ InflearnJavaAlgorithm/Section10_Dynamic_Programming/_4_Build_Tallest_Tower.java
@@ -35,56 +35,7 @@ import java.util.stream.Stream;
  * */
 
 
-class Brick implements Comparable<Brick>{
-    public int s, h, w;
-    Brick(int s, int h, int w) {
-        this.s = s;
-        this.h = h;
-        this.w = w;
-    }
-    @Override
-    public int compareTo(Brick o){
-        return o.s-this.s;
-    }
-}
-public class _4_Build_Tallest_Tower {
-    static int[] dy;
-    public int solution(ArrayList<Brick> arr){
-        int answer=0;
-        Collections.sort(arr);
-        dy[0]=arr.get(0).h;
-        answer=dy[0];
-        for(int i=1; i<arr.size(); i++){
-            int max_h=0;
-            for(int j=i-1; j>=0; j--){
-                if(arr.get(j).w > arr.get(i).w && dy[j]>max_h){
-                    max_h=dy[j];
-                }
-            }
-            dy[i]=max_h+arr.get(i).h;
-            answer=Math.max(answer, dy[i]);
-        }
-        return answer;
-    }
 
-    public static void main(String[] args){
-        _4_Build_Tallest_Tower T = new _4_Build_Tallest_Tower();
-        Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        ArrayList<Brick> arr=new ArrayList<>();
-        dy=new int[n];
-        for(int i=0; i<n; i++){
-            int a=kb.nextInt();
-            int b=kb.nextInt();
-            int c=kb.nextInt();
-            arr.add(new Brick(a, b, c));
-        }
-        System.out.print(T.solution(arr));
-    }
-}
-
-
-//===================Practice Code======================//
 class Brick_Info implements Comparable<Brick_Info>{
     int area; // 밑면의 넓이
     int height; // 높이
@@ -101,7 +52,8 @@ class Brick_Info implements Comparable<Brick_Info>{
         return o.area - this.area;
     }
 }
-class _4_Build_Tallest_Tower_Practice{
+
+public class _4_Build_Tallest_Tower{
 
     public int solution(ArrayList<Brick_Info> bricks){
         int dp[] = new int[bricks.size()];
@@ -122,7 +74,7 @@ class _4_Build_Tallest_Tower_Practice{
     }
 
     public static void main(String[] args) {
-        _4_Build_Tallest_Tower_Practice main = new _4_Build_Tallest_Tower_Practice();
+        _4_Build_Tallest_Tower main = new _4_Build_Tallest_Tower();
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
@@ -131,5 +83,56 @@ class _4_Build_Tallest_Tower_Practice{
             bricks.add(new Brick_Info(sc.nextInt(), sc.nextInt(), sc.nextInt()));
 
         System.out.println(main.solution(bricks));
+    }
+}
+
+//===================Inflearn Code======================//
+
+class Brick implements Comparable<Brick>{
+    public int s, h, w;
+    Brick(int s, int h, int w) {
+        this.s = s;
+        this.h = h;
+        this.w = w;
+    }
+    @Override
+    public int compareTo(Brick o){
+        return o.s-this.s;
+    }
+}
+
+class _4_Build_Tallest_Tower_Inflearn {
+    static int[] dy;
+    public int solution(ArrayList<Brick> arr){
+        int answer=0;
+        Collections.sort(arr);
+        dy[0]=arr.get(0).h;
+        answer=dy[0];
+        for(int i=1; i<arr.size(); i++){
+            int max_h=0;
+            for(int j=i-1; j>=0; j--){
+                if(arr.get(j).w > arr.get(i).w && dy[j]>max_h){
+                    max_h=dy[j];
+                }
+            }
+            dy[i]=max_h+arr.get(i).h;
+            answer=Math.max(answer, dy[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        _4_Build_Tallest_Tower_Inflearn T = new _4_Build_Tallest_Tower_Inflearn();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        ArrayList<Brick> arr=new ArrayList<>();
+        dy=new int[n];
+        for(int i=0; i<n; i++){
+            int a=kb.nextInt();
+            int b=kb.nextInt();
+            int c=kb.nextInt();
+            arr.add(new Brick(a, b, c));
+        }
+        System.out.print(T.solution(arr));
     }
 }
