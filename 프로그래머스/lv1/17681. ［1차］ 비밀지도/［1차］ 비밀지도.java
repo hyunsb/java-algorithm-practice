2c2b9f8c97a816b168675b3028d7e544b0112f22
정현수 <junghss0113@gmail.com>
2023-04-13 20:03:28 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.91 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] solution(int n, int[] arr, int[] arr2){
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String binaryString = Integer.toBinaryString(arr[i] | arr2[i]);
+
+            binaryString = String.format("%" + n + "s", binaryString);
+
+            binaryString = binaryString.replace("1", "#");
+            binaryString = binaryString.replace("0", " ");
+            answer[i] = binaryString;
+        }
+        
+        return answer;
+    }
+}
