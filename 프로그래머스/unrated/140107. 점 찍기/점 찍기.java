8f02c4eacb98d5aec7f4dadb50cdb6916d543b89
정현수 <junghss0113@gmail.com>
2023-04-22 21:21:18 +0900

    [unrated] Title: 점 찍기, Time: 0.13 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        for(int x=0; x<=d; x+=k){
+            int maxY = (int)Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2));
+            
+            answer += maxY/k + 1;
+        }
+        return answer;
+    }
+}
