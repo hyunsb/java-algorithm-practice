7fe6b4c63bac03b30f2da3f5533c4009450db3f8
정현수 <junghss0113@gmail.com>
2023-05-13 16:08:59 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int repeatCount = Integer.parseInt(bufferedReader.readLine());
+        int n = Integer.parseInt(bufferedReader.readLine());
 
-        for (int i = 0; i < repeatCount; i++) {
+        for (int i = 0; i < n; i++) {
+
+            Stack<Character> ps = new Stack<>();
             String answer = "NO";
             boolean flag = true;
-            String ps = bufferedReader.readLine();
-            Stack<Character> psStack = new Stack<>();
 
-            for(char parenthesis : ps.toCharArray()){
-                if(parenthesis == '(') {
-                    psStack.push(parenthesis);
-                }else {
-                    if(psStack.isEmpty()) {
-                        flag = false;
-                        break;
-                    }
-                    else psStack.pop();
+            for (Character c : bufferedReader.readLine().toCharArray()) {
+                try {
+                    Character character = (c == '(') ? ps.push(c) : ps.pop();
+                } catch (EmptyStackException error) {
+                    flag = false;
+                    break;
                 }
             }
-            if(flag && psStack.isEmpty()) answer = "YES";
+
+            if (flag && ps.isEmpty()) answer = "YES";
             System.out.println(answer);
         }
     }
