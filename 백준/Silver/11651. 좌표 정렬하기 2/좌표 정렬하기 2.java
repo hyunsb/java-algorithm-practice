37931691153cd49c2c5de24e7b8e7fcc85c832b3
정현수 <junghss0113@gmail.com>
2023-05-15 16:48:38 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 660 ms, Memory: 47516 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        int N = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine());
+            pq.add(new Point(Integer.parseInt(tokenizer.nextToken()), Integer.parseInt(tokenizer.nextToken())));
+        }
+        StringBuilder builder = new StringBuilder();
+        while (!pq.isEmpty()){
+            Point point = pq.poll();
+            builder.append(point.x).append(" ").append(point.y).append("\n");
+        }
+        System.out.println(builder);
+    }
+
+    private static class Point implements Comparable<Point>{
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return (y==o.y) ? x - o.x : y - o.y;
+        }
+    }
+}
