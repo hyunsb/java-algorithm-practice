bf431c347d545ed26716f7774aec782d3e65edb8
정현수 <junghss0113@gmail.com>
2023-05-17 13:43:03 +0900

    [level 2] Title: 가장 큰 수, Time: 3.68 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/README.md
+++ 프로그래머스/lv2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
 
 ### 성능 요약
 
-메모리: 79.8 MB, 시간: 5.64 ms
+메모리: 72.4 MB, 시간: 3.68 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -10,19 +10,10 @@ class Solution {
                 .mapToObj(Integer::toString)
                 .collect(Collectors.toList());
 
-        int cnt = 0;
-        for (String s : strings) {
-            if (s.equals("0"))
-                cnt++;
-        }
-        if (cnt == strings.size()){
-            return "0";
-        }
-
         strings.stream()
                 .sorted((s1, s2) -> (s2+s1).compareTo(s1+s2))
                 .forEach(builder::append);
 
-        return builder.toString();
+        return (builder.toString().charAt(0) == '0') ? "0" : builder.toString();
     }
 }
