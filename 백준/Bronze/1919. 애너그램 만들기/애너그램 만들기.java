f2b19e5f98f8c7c4e3a8385b3e4d5c80ffe0151b
정현수 <junghss0113@gmail.com>
2023-11-22 17:52:00 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 132 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,8 +1,6 @@
-import javax.swing.plaf.IconUIResource;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -11,33 +9,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String str1 = bufferedReader.readLine();
-        String str2 = bufferedReader.readLine();
+        String a = bufferedReader.readLine();
+        String b = bufferedReader.readLine();
 
-        Set<Character> set = new HashSet<>();
-        for (char c : (str1 + str2).toCharArray()) {
-           set.add(c);
+        int[] countA = new int[26];
+        int[] countB = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            countA[a.charAt(i) - 'a'] += 1;
         }
-        Map<Character, Integer> map1 = toMap(str1);
-        Map<Character, Integer> map2 = toMap(str2);
+
+        for (int i = 0; i < b.length(); i++) {
+            countB[b.charAt(i) - 'a'] += 1;
+        }
 
         int count = 0;
-        for (Character c : set) {
-            Integer i1 = map1.getOrDefault(c, 0);
-            Integer i2 = map2.getOrDefault(c, 0);
-
-            if (!i1.equals(i2)){
-                count += Math.abs(i1 - i2);
+        for (int i = 0; i < countA.length; i++) {
+            if (countA[i] != countB[i]) {
+                count += Math.abs(countA[i] - countB[i]);
             }
         }
         System.out.println(count);
-    }
-
-    public static Map<Character, Integer> toMap(String str) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : str.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        return map;
     }
 }
