3da0d1f8d3977c3df1f3c95edee0cdda679c3610
hyunsb <junghss0113@gmail.com>
2023-11-28 22:59:22 +0900

    인프런 String 5 solved

--- inflearn/string/string_5/Main.java
+++ inflearn/string/string_5/Main.java
@@ -10,22 +10,47 @@ public class Main {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String input = bufferedReader.readLine();
 
+        System.out.println(solutionB(input));
+    }
+
+    public static String solutionB(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        char[] words = word.toCharArray();
+
+        while (left < right) {
+            if (!Character.isAlphabetic(words[left])) left += 1;
+            if (!Character.isAlphabetic(words[right])) right -= 1;
+            if (Character.isAlphabetic(words[left]) && Character.isAlphabetic(words[right])) {
+                char temp = words[left];
+                words[left] = words[right];
+                words[right] = temp;
+
+                left += 1;
+                right -= 1;
+            }
+        }
+        return String.valueOf(words);
+    }
+
+    public static String solutionA(String word) {
         Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
+        for (char c : word.toCharArray()) {
             if (Character.isAlphabetic(c)) {
                 stack.push(c);
             }
         }
 
         StringBuilder stringBuilder = new StringBuilder();
-        char[] charArray = input.toCharArray();
-        for (int i = 0; i < input.length(); i++) {
+        char[] charArray = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
             if (Character.isAlphabetic(charArray[i])) {
                 stringBuilder.append(stack.pop());
             } else {
                 stringBuilder.append(charArray[i]);
             }
         }
-        System.out.println(stringBuilder);
+        return stringBuilder.toString();
     }
 }
