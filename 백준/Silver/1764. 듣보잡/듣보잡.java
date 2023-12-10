6d07dc25531a0b4ee34cc5c4968b5cb36f5b1803
정현수 <junghss0113@gmail.com>
2023-12-11 00:49:23 +0900

    [Silver IV] Title: 듣보잡, Time: 428 ms, Memory: 26564 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,29 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int m = Integer.parseInt(stringTokenizer.nextToken());
 
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < n + m; i++) {
-            String name = bufferedReader.readLine();
-            map.put(name, map.getOrDefault(name, 0) + 1);
-        }
-
+        Set<String> set = new HashSet<>();
         Set<String> answer = new TreeSet<>();
-        for (String s : map.keySet()) {
-            if(map.get(s) > 1) {
-                answer.add(s);
+        
+        for (int i = 0; i < n; i++) {
+            set.add(bufferedReader.readLine());
+        }
+        for (int i = 0; i < m; i++) {
+            String name = bufferedReader.readLine();
+            if (set.contains(name)) {
+                answer.add(name);
             }
         }
+
         System.out.println(answer.size());
         answer.forEach(System.out::println);
     }
