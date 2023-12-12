350a94ba20be5f2a55d1fb34baeae85a0b55a208
hyunsb <junghss0113@gmail.com>
2023-12-13 00:04:52 +0900

    [인프런] Sorting and Searching - 버블 정렬

---
+++ inflearn/sorting_searching/_2/Main.java
@@ -0,0 +1,33 @@
+package inflearn.sorting_searching._2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[] arr = new int[n];
+        StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
