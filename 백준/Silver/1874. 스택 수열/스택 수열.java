622c7a5f4aadaf1163ba19291b28a96757fccf83
정현수 <junghss0113@gmail.com>
2023-12-13 02:34:56 +0900

    [Silver II] Title: 스택 수열, Time: 360 ms, Memory: 26888 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        StringBuilder answer = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        int i = 1;
+        for (int repeat = 0; repeat < n; repeat++) {
+            int target = Integer.parseInt(bufferedReader.readLine());
+            while (target >= i) {
+                stack.push(i++);
+                answer.append("+\n");
+            }
+
+            if (stack.peek() == target) {
+                stack.pop();
+                answer.append("-\n");
+            } else {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println(answer);
+    }
+}
