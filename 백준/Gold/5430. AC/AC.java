13b0fb5c919aa480152a2cc13e8d28e3c2fd4aec
정현수 <junghss0113@gmail.com>
2024-01-20 00:13:51 +0900

    [Gold V] Title: AC, Time: 632 ms, Memory: 69512 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -4,33 +4,33 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    
+
     static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder answer = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
 
-        ArrayDeque<Integer> deque;
+        ArrayDeque<String> deque;
         StringTokenizer tokenizer;
-        
+
         int loof = Integer.parseInt(bufferedReader.readLine());
 
         while (loof-- > 0) {
-            
+
             char[] commands = bufferedReader.readLine().toCharArray();
             int n = Integer.parseInt(bufferedReader.readLine());
             tokenizer = new StringTokenizer(bufferedReader.readLine(), "[],");
-            
+
             deque = new ArrayDeque<>();
             for (int i = 0; i < n; i++) {
-                deque.add(Integer.parseInt(tokenizer.nextToken()));
+                deque.add(tokenizer.nextToken());
             }
             AC(deque, commands);
         }
         System.out.println(answer);
     }
 
-    private static void AC(ArrayDeque<Integer> deque, char[] commands) {
+    private static void AC(ArrayDeque<String> deque, char[] commands) {
         boolean isRight = true;
         for (char command : commands) {
             if (command == 'R') {
@@ -45,7 +45,6 @@ public class Main {
                 }
             }
             else {
-                // 만약 반환 된 원소가 없을 경우 error를 출력하도록 하고 함수 종료
                 if(deque.pollLast() == null) {
                     answer.append("error\n");
                     return;
@@ -55,31 +54,22 @@ public class Main {
         makePrintString(deque, isRight);
     }
 
-    public static void makePrintString(ArrayDeque<Integer> deque, boolean isRight) {
-
-        answer.append('[');	// 여는 대괄호 먼저 StringBuilder에 저장
-
-        if(deque.size() > 0) {	//만약 출력 할 원소가 한 개 이상일 경우
-
-            if(isRight) {	// 정방향일경우
-
-                answer.append(deque.pollFirst());	// 먼저 첫 번째 원소를 넘겨준다.
-
-                // 그 다음 원소부터 반점을 먼저 넘겨준 후 덱의 원소를 하나씩 뽑아 넘긴다.
+    public static void makePrintString(ArrayDeque<String> deque, boolean isRight) {
+        answer.append('[');
+        if(deque.size() > 0) {
+            if(isRight) {
+                answer.append(deque.pollFirst());
                 while(!deque.isEmpty()) {
                     answer.append(',').append(deque.pollFirst());
                 }
             }
-            else {	// 역방향일경우
-                answer.append(deque.pollLast());	// 먼저 뒤에서부터 첫 번째 원소를 넘겨준다.
-
-                // 그 다음 원소부터 반점을 먼저 넘겨준 후 덱의 원소를 뒤에서부터 하나씩 뽑아 넘긴다.
+            else {
+                answer.append(deque.pollLast());
                 while(!deque.isEmpty()) {
                     answer.append(',').append(deque.pollLast());
                 }
             }
         }
-
-        answer.append(']').append('\n');	// 닫는 대괄호 및 개행으로 마무리
+        answer.append(']').append('\n');
     }
 }
