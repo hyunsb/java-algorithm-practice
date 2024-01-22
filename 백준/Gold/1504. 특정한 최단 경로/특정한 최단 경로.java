080f7e0762f12e806096ca786314a4578933b677
정현수 <junghss0113@gmail.com>
2024-01-22 22:51:52 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 580 ms, Memory: 69148 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,117 +1,114 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
-class Node implements Comparable<Node> {
-    int end;
-    int weight;
-
-    Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
-
-}
+import java.util.*;
 
 public class Main {
-    static int N, E;
-    static ArrayList<ArrayList<Node>> a; // 인접리스트.
-    static int[] dist; // 시작점에서 각 정점으로 가는 최단거리.
-    static boolean[] check; // 방문 확인.
-    static final int INF = 200000000;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+    static int n;
 
-        a = new ArrayList<>();
-        dist = new int[N + 1];
-        check = new boolean[N + 1];
+    static List<Node>[] map;
+    static boolean[] isVisited;
 
-        Arrays.fill(dist, INF);
+    static int[] minDistanceFromStart;
+    static int[] minDistanceFromV1;
+    static int[] minDistanceFromV2;
 
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
-        }
+    private static void dijkstra(int start, int[] minDistance, int ... ends) {
+        Queue<Node> queue = new PriorityQueue<>();
+        isVisited = new boolean[n+1];
+        queue.add(new Node(start, 0));
+        minDistance[start] = 0;
 
-        // 양방향 인접 리스트 구현.
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            int currentVertex = current.vertex;
 
-            // start에서 end로 가는 weight (가중치)
-            a.get(start).add(new Node(end, weight));
+            if (isVisited[currentVertex]) continue;
+            isVisited[currentVertex] = true;
 
-            // end에서 start로 가는 weight (가중치)
-            a.get(end).add(new Node(start, weight));
-        }
+            boolean isDone = true;
+            for (int end : ends) {
+                if (!isVisited[end]) isDone = false;
+            }
+            if (isDone) return;
 
-        // 반드시 거쳐야 하는 정점.
-        st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
+            for (Node node : map[currentVertex]) {
+                int nextVertex = node.vertex;
+                int nextWeight = node.weight + minDistance[currentVertex];
 
-        // 1 -> v1 -> v2 -> N으로 가는 경우
-        int res1 = 0;
-        res1 += dijkstra(1, v1);
-        res1 += dijkstra(v1, v2);
-        res1 += dijkstra(v2, N);
-
-        // 1 -> v2 -> v1 -> N으로 가는 경우
-        int res2 = 0;
-        res2 += dijkstra(1, v2);
-        res2 += dijkstra(v2, v1);
-        res2 += dijkstra(v1, N);
-
-        int ans = (res1 >= INF && res2 >= INF) ? -1 : Math.min(res1, res2);
-
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    // 다익스트라 알고리즘
-    public static int dijkstra(int start, int end) {
-        Arrays.fill(dist, INF);
-        Arrays.fill(check, false);
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        boolean[] check = new boolean[N + 1];
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-            int cur = curNode.end;
-
-            if (!check[cur]) {
-                check[cur] = true;
-
-                for (Node node : a.get(cur)) {
-                    if (!check[node.end] && dist[node.end] > dist[cur] + node.weight) {
-                        dist[node.end] = dist[cur] + node.weight;
-                        pq.add(new Node(node.end, dist[node.end]));
-                    }
+                if (nextWeight < minDistance[nextVertex]) {
+                    minDistance[nextVertex] = nextWeight;
+                    queue.add(new Node(nextVertex, nextWeight));
                 }
             }
         }
+    }
 
-        return dist[end];
+    private static class Node implements Comparable<Node> {
+        int vertex;
+        int weight;
+
+        public Node(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node that) {
+            return this.weight - that.weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine());
+        int INF = 2_000_000_000;
+
+        n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+
+        map = new ArrayList[n+1];
+        for (int i = 0; i < n + 1; i++) {
+            map[i] = new ArrayList<>();
+        }
+
+        minDistanceFromStart = new int[n+1];
+        minDistanceFromV1 = new int[n+1];
+        minDistanceFromV2 = new int[n+1];
+        Arrays.fill(minDistanceFromStart, INF);
+        Arrays.fill(minDistanceFromV1, INF);
+        Arrays.fill(minDistanceFromV2, INF);
+
+        for (int i = 0; i < m; i++) {
+            tokenizer = new StringTokenizer(bufferedReader.readLine());
+            int start = Integer.parseInt(tokenizer.nextToken());
+            int end = Integer.parseInt(tokenizer.nextToken());
+            int weight = Integer.parseInt(tokenizer.nextToken());
+
+            map[start].add(new Node(end, weight));
+            map[end].add(new Node(start, weight));
+        }
+
+        tokenizer = new StringTokenizer(bufferedReader.readLine());
+        int v1 = Integer.parseInt(tokenizer.nextToken());
+        int v2 = Integer.parseInt(tokenizer.nextToken());
+
+
+        dijkstra(1, minDistanceFromStart, v1, v2);
+        dijkstra(v1, minDistanceFromV1, v2, n);
+        dijkstra(v2, minDistanceFromV2, v1, n);
+
+        long selectV1 = 0;
+        selectV1 += minDistanceFromStart[v1];
+        selectV1 += minDistanceFromV1[v2];
+        selectV1 += minDistanceFromV2[n];
+        
+        long selectV2 = 0;
+        selectV2 += minDistanceFromStart[v2];
+        selectV2 += minDistanceFromV2[v1];
+        selectV2 += minDistanceFromV1[n];
+        
+        System.out.println((selectV1 >= INF && selectV2 >= INF) ? -1 : Math.min(selectV1, selectV2));
     }
 }
