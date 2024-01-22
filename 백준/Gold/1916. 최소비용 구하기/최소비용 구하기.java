81cfd7b816e9e62591794d6eedaa76d55101be9d
정현수 <junghss0113@gmail.com>
2024-01-22 20:19:01 +0900

    [Gold V] Title: 최소비용 구하기, Time: 568 ms, Memory: 50916 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+    private static int INF = 1_000_000_000;
+    private static List<Node>[] map;
+    private static boolean[] isVisited;
+    private static int[] minDistance;
+
+    private static void dijkstra(int start) {
+        Queue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+        minDistance[start] = 0;
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            int currentVertex = current.vertex;
+
+            if (isVisited[currentVertex]) continue;
+            isVisited[currentVertex] = true;
+
+            for (Node node : map[currentVertex]) {
+                int nextVertex = node.vertex;
+                int nextWeight = node.weight + minDistance[currentVertex];
+                if (minDistance[nextVertex] > nextWeight) {
+                    minDistance[nextVertex] = nextWeight;
+                    queue.offer(new Node(nextVertex, nextWeight));
+                }
+            }
+        }
+    }
+
+    private static class Node implements Comparable<Node> {
+        int vertex;
+        int weight;
+
+        public Node(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node that) {
+            return this.weight - that.weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int m = Integer.parseInt(bufferedReader.readLine());
+
+        map = new ArrayList[n+1];
+        isVisited = new boolean[n+1];
+        minDistance = new int[n+1];
+
+        Arrays.fill(minDistance, INF);
+        for (int i = 0; i < n + 1; i++) {
+            map[i] = new ArrayList<>();
+        }
+
+        StringTokenizer tokenizer;
+        for (int i = 0; i < m; i++) {
+            tokenizer = new StringTokenizer(bufferedReader.readLine());
+            int root = Integer.parseInt(tokenizer.nextToken());
+            int vertex = Integer.parseInt(tokenizer.nextToken());
+            int weight = Integer.parseInt(tokenizer.nextToken());
+
+            map[root].add(new Node(vertex, weight));
+        }
+
+        tokenizer = new StringTokenizer(bufferedReader.readLine());
+        int start = Integer.parseInt(tokenizer.nextToken());
+        int end = Integer.parseInt(tokenizer.nextToken());
+
+        dijkstra(start);
+        System.out.println(minDistance[end]);
+    }
+}
