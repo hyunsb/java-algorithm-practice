33c3dd27f09b4c881ffdd96e19c83bb9b8ab8c0d
정현수 <junghss0113@gmail.com>
2024-01-25 02:41:04 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 576 ms, Memory: 51152 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,89 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
- 
-public class Main {
-    
-    static ArrayList<Node>[] list;
-    static int n, m, start, end; 
-    static int[] dist;
-    static int[] route; // 직전 노드 저장
-    static boolean[] visited; 
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        n = scan.nextInt();
-        m = scan.nextInt();
-        
-        list = new ArrayList[n + 1];
-        for(int i = 1; i <= n; i++) {
-            list[i] = new ArrayList<>();
-        }
-        
-        for(int i = 0; i < m; i++) {
-            int s = scan.nextInt();
-            int e = scan.nextInt();
-            int c = scan.nextInt();
-            list[s].add(new Node(e, c));
-        }
-        
-        start = scan.nextInt();
-        end = scan.nextInt();
-        
-        dist = new int[n + 1];
-        route = new int[n + 1];
-        Arrays.fill(dist, 1000000001);
-        visited = new boolean[n + 1];
-        dijkstra();
-        
-        System.out.println(dist[end]);
-        
-        ArrayList<Integer> routes = new ArrayList<>();
-        int current = end;
-        while(current != 0) {
-            routes.add(current);
-            current = route[current];
-        }
-        System.out.println(routes.size());
-        for(int i = routes.size() - 1; i >= 0; i--) {
-            System.out.print(routes.get(i) + " ");
-        }
-    }
-    
-    public static void dijkstra() {
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        q.add(new Node(start, 0));
-        dist[start] = 0;
-        route[start] = 0;
-        
-        while(!q.isEmpty()) {
-            Node current = q.poll();
-            
-            if(!visited[current.e]) visited[current.e] = true;
-            else continue;
-            
-            for(int i = 0; i < list[current.e].size(); i++) {
-                Node next = list[current.e].get(i);
-                if(dist[next.e] > dist[current.e] + next.cost) {
-                    dist[next.e] = dist[current.e] + next.cost;
-                    q.offer(new Node(next.e, dist[next.e]));
-                    route[next.e] = current.e;
+
+class Main {
+
+    static List<Node>[] map;
+    static int n, m, start, end, INF = 100_000_000;
+    static int[] minDistance, preVisited;
+    static boolean[] isVisited;
+
+    private static void dijkstra() {
+        Queue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+        minDistance[start] = 0;
+        preVisited[start] = 0;
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            int currentVertex = current.vertex;
+
+            if (isVisited[currentVertex]) continue;
+            isVisited[currentVertex] = true;
+
+            for (Node next : map[currentVertex]) {
+                int nextVertex = next.vertex;
+                int nextWeight = next.weight + minDistance[currentVertex];
+                if (nextWeight < minDistance[nextVertex]) {
+                    minDistance[nextVertex] = nextWeight;
+                    queue.add(new Node(nextVertex, nextWeight));
+                    preVisited[nextVertex] = currentVertex;
                 }
             }
         }
     }
-    
-    public static class Node implements Comparable<Node> {
-        int e;
-        int cost;
-        
-        public Node(int e, int cost) {
-            this.e = e;
-            this.cost = cost;
+
+    private static class Node implements Comparable<Node> {
+        int vertex;
+        int weight;
+
+        public Node(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
         }
-        
+
         @Override
-        public int compareTo(Node n) {
-            return this.cost - n.cost;
+        public int compareTo(Node that) {
+            return this.weight - that.weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bufferedReader.readLine());
+        m = Integer.parseInt(bufferedReader.readLine());
+
+        map = new List[n+1];
+        for (int i = 1; i < n + 1; i++) {
+            map[i] = new ArrayList<>();
+        }
+
+        StringTokenizer tokenizer;
+        for (int i = 0; i < m; i++) {
+            tokenizer = new StringTokenizer(bufferedReader.readLine());
+            int start = Integer.parseInt(tokenizer.nextToken());
+            int end = Integer.parseInt(tokenizer.nextToken());
+            int weight = Integer.parseInt(tokenizer.nextToken());
+
+            map[start].add(new Node(end, weight));
+        }
+
+        tokenizer = new StringTokenizer(bufferedReader.readLine());
+        start = Integer.parseInt(tokenizer.nextToken());
+        end = Integer.parseInt(tokenizer.nextToken());
+
+        isVisited = new boolean[n+1];
+        preVisited = new int[n+1];
+        minDistance = new int[n+1];
+        Arrays.fill(minDistance, INF);
+        dijkstra();
+
+        System.out.println(minDistance[end]);
+
+        Stack<Integer> routes = new Stack<>();
+        int current = end;
+        while (current != 0) {
+            routes.add(current);
+            current = preVisited[current];
+        }
+        System.out.println(routes.size());
+        while (!routes.isEmpty()) {
+            System.out.print(routes.pop() + " ");
         }
     }
 }
