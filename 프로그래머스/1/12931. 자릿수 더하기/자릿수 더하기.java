50fa9ce5c602f840075bf09e500fe58a5b90e82b
정현수 <junghss0113@gmail.com>
2024-11-12 10:10:41 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        
+        int answer = 0;
+        
+        int temp = n;
+        while (temp != 0) {
+            answer += temp % 10;
+            temp = temp / 10;
+        }
+
+        return answer;
+    }
+}
