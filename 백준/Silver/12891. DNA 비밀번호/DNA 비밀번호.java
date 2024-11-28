61dcac1e481762631905a933649601860a8bb1af
정현수 <junghss0113@gmail.com>
2024-11-28 10:38:22 +0900

    [Silver II] Title: DNA 비밀번호, Time: 464 ms, Memory: 70068 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -8,49 +8,48 @@ import java.util.StringTokenizer;
 
 class Main {
 
-    static List<String> DNAs = List.of("A", "C", "G", "T");
-    static Map<String, Integer> require = new HashMap<>();
-    static Map<String, Integer> temp = new HashMap<>();
+    static List<Character> DNAs = List.of('A', 'C', 'G', 'T');
+    static Map<Character, Integer> require = new HashMap<>();
     static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine());
+
         int wordLength = Integer.parseInt(tokenizer.nextToken());
         int passwordLength = Integer.parseInt(tokenizer.nextToken());
-        String word = bufferedReader.readLine();
+        String words = bufferedReader.readLine();
 
         tokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (String dna : DNAs) {
+        for (Character dna : DNAs) {
             require.put(dna, Integer.parseInt(tokenizer.nextToken()));
-            temp.put(dna, 0);
         }
 
-        String[] words = word.split("");
         for (int i = 0; i < passwordLength; i++) {
-            String target = words[i];
-            temp.put(target, temp.get(target) + 1);
+            char target = words.charAt(i);
+            require.put(target, require.get(target) - 1);
         }
         if (checkRequire()) count += 1;
 
         for (int i = passwordLength; i < wordLength; i++) {
-            String prev = words[i-passwordLength];
-            String next = words[i];
+            char prev = words.charAt(i - passwordLength);
+            char post = words.charAt(i);
 
-            temp.put(prev, temp.get(prev) - 1);
-            temp.put(next, temp.get(next) + 1);
+            require.put(prev, require.get(prev) + 1);
+            require.put(post, require.get(post) - 1);
 
-            if (checkRequire()) count += 1;
+            if (checkRequire()) {
+                count += 1;
+            }
         }
-
         System.out.println(count);
     }
 
     static boolean checkRequire() {
-        for (String dna : DNAs) {
-            int req = require.get(dna);
-            int target = temp.get(dna);
-            if (target < req) return false;
+        for (Character key : require.keySet()) {
+            if (require.get(key) > 0) {
+                return false;
+            }
         }
         return true;
     }
