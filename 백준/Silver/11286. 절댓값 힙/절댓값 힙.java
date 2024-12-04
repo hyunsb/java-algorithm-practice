a0479a889f4b2f4e11bd2d33339080bce0a77a71
정현수 <junghss0113@gmail.com>
2024-12-04 10:47:10 +0900

    [Silver I] Title: 절댓값 힙, Time: 300 ms, Memory: 25520 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,27 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.PriorityQueue;
+import java.util.Queue;
+
+;
 
 class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(bufferedReader.readLine());
-
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> {
-            if (Math.abs(o1) == Math.abs(o2)) return o1 - o2;
-            else return Math.abs(o1) - Math.abs(o2);
+        int repeat = Integer.parseInt(bufferedReader.readLine());
+        Queue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            if (o1 + o2 == 0) {
+                return o1 > o2 ? 1 : -1;
+            }
+            return Math.abs(o1) - Math.abs(o2);
         });
 
         StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int number = Integer.parseInt(bufferedReader.readLine());
-            if (number != 0) {
-                priorityQueue.add(number);
+        for (int i = 0; i < repeat; i++) {
+            String operation = bufferedReader.readLine();
+
+            if (Objects.equals(operation, "0")) {
+                if (queue.isEmpty()) {
+                    answer.append("0\n");
+                } else {
+                    answer.append(queue.poll()).append("\n");
+                }
+
             } else {
-                if (priorityQueue.isEmpty()) answer.append(0).append("\n");
-                else answer.append(priorityQueue.poll()).append("\n");
+                queue.offer(Integer.parseInt(operation));
             }
         }
         System.out.println(answer);
