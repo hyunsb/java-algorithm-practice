105ac32d37e7fa4dad959cfeebf9d3e62ed6a147
정현수 <junghss0113@gmail.com>
2024-12-19 16:05:51 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 24136 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -6,26 +6,27 @@ import java.util.StringTokenizer;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int s = Integer.parseInt(tokenizer.nextToken());
+        int[] arr = new int[n+1];
 
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        int s = Integer.parseInt(stringTokenizer.nextToken());
-        int[] numbers = new int[n+1];
+        tokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(tokenizer.nextToken());
+        }
 
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < n; i++) numbers[i] = Integer.parseInt(stringTokenizer.nextToken());
-
-        int start = 0;
-        int end = 0;
-        int length = Integer.MAX_VALUE;
         int sum = 0;
+        int length = Integer.MAX_VALUE;
+        int start = 0, end = 0;
         while (start <= end && end <= n) {
             if (sum < s) {
-                sum += numbers[end++];
-            } else if (sum >= s) {
-                length = Math.min(length, end - start);
-                sum -= numbers[start++];
+                sum += arr[end++];
+            } else {
+                int cur = end - start;
+                if (cur < length) length = cur;
+                sum -= arr[start++];
             }
         }
         System.out.println(length == Integer.MAX_VALUE ? 0 : length);
