660aa417388117c0c4824a1e2c931bce0c76ea54
정현수 <junghss0113@gmail.com>
2025-02-20 10:55:57 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,28 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(tokenizer.nextToken());
-        int K = Integer.parseInt(tokenizer.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int money = Integer.parseInt(st.nextToken());
 
-        int[] coins = new int[N];
-        for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(bufferedReader.readLine());
+        List<Integer> coins = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            coins.add(Integer.parseInt(br.readLine()));
         }
+        
+        int count = 0;
+        for (int i = coins.size() - 1; i >= 0; i--) {
+            int coin = coins.get(i);
 
-        int answer = 0;
-        for (int i = coins.length-1; i >= 0; i--) {
-            int coin = coins[i];
-            answer += K / coin;
-            K %= coin;
+            if (money >= coin) {
+                int tempCount = (money / coin);
+                count += tempCount;
+                money -= tempCount * coin;
+            }
         }
-
-        System.out.println(answer);
+        System.out.println(count);
     }
 }
