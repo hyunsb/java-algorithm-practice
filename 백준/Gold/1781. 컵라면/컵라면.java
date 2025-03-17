a940f984b16de54bbcbe4a96ca585a2ae7579446
정현수 <junghss0113@gmail.com>
2025-03-17 12:04:03 +0900

    [Gold II] Title: 컵라면, Time: 740 ms, Memory: 72400 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -1,7 +1,14 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
@@ -13,38 +20,39 @@ class Main {
             tokenizer = new StringTokenizer(bufferedReader.readLine());
             int deadLine = Integer.parseInt(tokenizer.nextToken());
             int score = Integer.parseInt(tokenizer.nextToken());
+
             problems.add(new Problem(deadLine, score));
         }
+        problems.sort(Problem::compareTo);
 
-        // 데드라인 오름차순 정렬
-        problems.sort(Comparator.comparingInt(p -> p.deadLine));
+        Queue<Integer> queue = new PriorityQueue<>();
+        for (Problem problem : problems) {
+            queue.offer(problem.score);
 
-        // 점수 기준 최소 힙
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (Problem p : problems) {
-            pq.offer(p.score);
-            // 데드라인보다 푼 문제 수가 많으면 점수 낮은 것 제거
-            if (pq.size() > p.deadLine) {
-                pq.poll();  // 가장 점수 낮은 문제 제거
+            if (queue.size() > problem.deadLine) {
+                queue.poll();
             }
         }
 
-        // 총 점수 계산
-        int totalScore = 0;
-        while (!pq.isEmpty()) {
-            totalScore += pq.poll();
+        int answer = 0;
+        while (!queue.isEmpty()){
+            answer += queue.poll();
         }
-
-        System.out.println(totalScore);
+        System.out.println(answer);
     }
 
-    private static class Problem {
+    private static class Problem implements Comparable<Problem> {
+
         int deadLine, score;
 
         public Problem(int deadLine, int score) {
             this.deadLine = deadLine;
             this.score = score;
+        }
+
+        @Override
+        public int compareTo(Problem that) {
+            return this.deadLine - that.deadLine;
         }
     }
 }
